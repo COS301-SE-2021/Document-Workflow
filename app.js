9e64c28222bce2ab3ba9bf04e049a7ab74acacf6
delareytuks <u14011639@tuks.co.za>
2021-05-28 11:27:03 +0200

    removed book.html removed temporary code from app.js

--- app.js
+++ app.js
@@ -1,36 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+//const cors = require('cors');
 
 const app = express();
 const port = 3000;
 
-//let books = [];
+//app.use(cors());
 
- app.use(cors());
-
-//configure body-parser:
+//configure body-parser: (Middleware)
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+//Accepting anything, must validate:
 app.post('/book', (req,res) => {
     const book = req.body;
     console.log(book);
-
+    books.push(book);
     res.send('Book added to database');
 })
 
 app.get('/', (req,res) => {
     res.send('Hello world, from express');
 });
-
-app.get('/new-book', (req,res) => {
-    res.sendFile('testing/book.html', {root: __dirname})
-});
-
-app.get('/node_modules/bootstrap/dist/css/bootstrap.css', ((req, res) => {
-    res.sendFile('node_modules/bootstrap/dist/css/bootstrap.css', {root: __dirname})
-}));
 
 app.listen(port, () => {
     console.log(`Hello world app listening on port ${port}`)
