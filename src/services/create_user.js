980e9141f060438b62cb1a70af92af92e95860f5
u17112592 <u17112592@tuks.co.za>
2021-06-02 09:41:55 +0200

    Initial user registration functionality added: checks for all necessary fields are implemented and an error is thrown if a needed field is missing.

--- src/services/create_user.js
+++ src/services/create_user.js
@@ -1,2 +1,41 @@
 
 
+const http = require('http')
+const url = require('url')
+const MockDatabase = require('./MockDatabase')
+const Verifier = require("email-verifier")
+
+module.exports = {
+    handle: handle_create_user
+};
+
+/**
+ * Checks for input parameters based on the following order:
+ * id: search by id
+ * email: search by email address
+ * username: search by username
+ * If none of the above are specified, then an error message will be returned
+ * @param req
+ *
+ */
+function handle_create_user(req) {
+
+    const queryObject = url.parse(req.url, true).query
+
+    let names = ["Fname", "Lname", "email", "password", "phone_number"]
+    const data = {Fname: queryObject["Fname"], Lname:queryObject["Lname"], email:queryObject["email"],
+                password:queryObject["password"], phone_number:queryObject["phone_number"]}
+
+    for(let i=0; i<names.length;++i)  //check that all necessary fields have been set
+        if(data[names[i]] === undefined)
+            return createErrorResponse("Field '" + names[i] + "' not set")
+
+
+    return {status:"success","data":{}, message:"New user created"};
+}
+
+function createErrorResponse(error_message)
+{
+    return {status:"error",data:null,"message":error_message}
+}
+
