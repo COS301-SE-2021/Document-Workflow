c9229aef1dd289b9ff05eca3286cb53f9cbd943f
u17112592 <u17112592@tuks.co.za>
2021-06-02 12:37:56 +0200

    Created the login_user.js file to handle the process of logging in a user.

---
+++ src/services/login_user.js
@@ -0,0 +1,14 @@
+
+const http = require('http')
+const url = require('url')
+const MockDatabase = require('./MockDatabase')
+
+module.exports = {
+    handle: handle_login_user
+};
+
+
+function handle_login_user(req)
+{
+
+}
