02ec35f9deac5e0881dcbc18055f8981590f75f7
u17112592 <u17112592@tuks.co.za>
2021-06-02 09:39:25 +0200

    Created a MockDatabase file that will be used to test login and registration functions until such a point that the database is set up.

---
+++ src/services/MockDatabase.js
@@ -0,0 +1,23 @@
+
+let database = {}; //will just be a list of signatures
+
+class User{
+
+    static next_ID = 0
+
+    constructor(Fname, Lname, email, password, signature, phone_number) {
+        this.Fname = Fname;
+        this.Lname = Lname;
+        this.email = email;
+        this.password  = password;
+        this.signature = signature;
+        this.phone_number = phone_number
+        this.ID = User.next_ID
+        User.next_ID ++
+    }
+
+}
+
+
+exports.User = User
+exports.database = database
