cb1d9507366bbb0f45127da97613b459957ae20d
u17112592 <u17112592@tuks.co.za>
2021-06-02 12:47:01 +0200

    Added checks for necessary parameters in the login_request.

--- src/services/login_user.js
+++ src/services/login_user.js
@@ -2,6 +2,7 @@
 const http = require('http')
 const url = require('url')
 const MockDatabase = require('./MockDatabase')
+const create_user = require('./create_user')
 
 module.exports = {
     handle: handle_login_user
@@ -9,6 +10,20 @@ module.exports = {
 
 
 function handle_login_user(req)
+{
+    const queryObject = url.parse(req.url, true).query
+    let email = queryObject["email"]
+    let password = queryObject["password"]
+
+    if(email === undefined)
+        return {"status":"error","data":null,"message":"Field 'email' not set"}
+    if(password === undefined)
+        return {"status":"error","data":null,"message":"Field 'password' not set"}
+
+    return {status:"success","data":{}, message:"Fields all set"}
+}
+
+function getUserByEmail(email)
 {
 
 }
