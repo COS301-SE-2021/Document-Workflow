b2cd8cd7ac3ce292f582350442116a137fe09c04
u17112592 <u17112592@tuks.co.za>
2021-06-02 12:59:37 +0200

    Completed the prototype login functionality.

--- src/services/login_user.js
+++ src/services/login_user.js
@@ -8,7 +8,12 @@ module.exports = {
     handle: handle_login_user
 };
 
-
+/**
+ * Very important that we send the same response for an incorrect password and an incorrect email address
+ * otherwise it could lead to a security vulnerability.
+ * @param req
+ * @returns {{data: null, message: string, status: string}|{data: {}, message: string, status: string}}
+ */
 function handle_login_user(req)
 {
     const queryObject = url.parse(req.url, true).query
@@ -20,10 +25,26 @@ function handle_login_user(req)
     if(password === undefined)
         return {"status":"error","data":null,"message":"Field 'password' not set"}
 
-    return {status:"success","data":{}, message:"Fields all set"}
+    let user = getUserByEmail(email)
+
+    if(user === null)
+        return {"status":"error","data":null,"message":"Email address or password incorrect"}
+
+    let hashed_password = create_user.hashAndSaltPassword(password)
+
+    if(hashed_password !== user.password)
+        return {"status":"error","data":null,"message":"Email address or password incorrect"}
+
+    return {status:"success","data":{"ID": user.ID}, message:"Successful Login"} //TODO: should we return the User's ID? probably not
 }
 
 function getUserByEmail(email)
 {
+    for(let i=0; i<Object.getOwnPropertyNames(MockDatabase.database).length; ++i)
+    {
+        if(MockDatabase.database[i].email === email)
+            return MockDatabase.database[i]
+    }
 
+    return null
 }
