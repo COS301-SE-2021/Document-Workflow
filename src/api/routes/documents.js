3289b79749e1a7acfc89ee12139ae9a6f8ed7923
delareytuks <u14011639@tuks.co.za>
2021-06-03 14:43:14 +0200

    added endpoints for /api/documents

---
+++ src/api/routes/documents.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const Doc = require("../../models/user");
+const router = express.Router();
+
+router.post('', (req, res) => {
+    console.log(req.body);
+    const document = new Doc({
+        userId: req.body.userID,
+        doc_name: req.body.documentName,
+        location: req.body.documentLocation,
+        status: req.body.status,
+        type: req.body.phone,
+        description: req.body.phone
+    });
+    console.log(document);
+    console.log(document);
+    document.save()
+        .then((doc)=>{
+            res.status(200).json({
+                message: "User added successfully",
+                docId: doc._id
+            });
+        })
+        .catch((msg)=>{
+            res.status(500).json({
+                message: msg
+            });
+        });
+});
+
+module.exports = router;
