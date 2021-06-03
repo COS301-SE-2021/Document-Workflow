f5187f782bcb07d0a56f98351338ccb690ab92b6
delareytuks <u14011639@tuks.co.za>
2021-06-03 21:10:55 +0200

    fixed path to schema import

--- src/api/routes/documents.js
+++ src/api/routes/documents.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const Doc = require("../../models/document");
+const Doc = require("../../schemas/document");
 const mongoose = require('mongoose')
 const router = express.Router();
 
--- src/api/routes/users.js
+++ src/api/routes/users.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const User = require("../../models/user");
+const User = require("../../schemas/user");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
 const saltRounds = process.env.SALT_ROUNDS;
