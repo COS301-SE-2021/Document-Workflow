a00e50f3fd064cc79e59e07bf8dbdeba27186bdf
Mosa <letswalomosa@gmail.com>
2021-06-03 17:58:30 +0200

    Update view-document-modal.module.ts
    download function implementation

--- app/src/app/Modals/view-document-modal/view-document-modal.module.ts
+++ app/src/app/Modals/view-document-modal/view-document-modal.module.ts
@@ -8,6 +8,11 @@ import { ViewDocumentModalPageRoutingModule } from './view-document-modal-routin
 
 import { ViewDocumentModalPage } from './view-document-modal.page';
 
+import { File } from "@ionic-native/file/ngx";
+import { FileTransfer, FileTransferObject } from "@ionic-native/file-transfer/ngx";
+import { FileOpener } from "@ionic-native/file-opener/ngx";
+import { Injectable } from "@angular/core";
+
 @NgModule({
   imports: [
     CommonModule,
@@ -18,3 +23,31 @@ import { ViewDocumentModalPage } from './view-document-modal.page';
   declarations: [ViewDocumentModalPage]
 })
 export class ViewDocumentModalPageModule {}
+
+
+@Injectable
+({
+  providedIn: "root"
+})
+
+export class DocumentService {
+  fileTransfer: FileTransferObject;
+  constructor(
+    private fileOpener: FileOpener,
+    private transfer: FileTransfer,
+    private file: File
+  ) {}
+
+  download(url: string, title: string)
+  {
+    this.fileTransfer = this.transfer.create();
+    this.fileTransfer.download(url, this.file.dataDirectory + title + ".pdf")
+      .then(entry =>
+      {
+        console.log("downloaded successfully: " + entry.toURL());
+        this.fileOpener
+          .open(entry.toURL(), "application/pdf")
+          .then(() => console.log("File is opened"))
+          .catch(e => console.log("Error opening file", e));
+      });
+  }}
