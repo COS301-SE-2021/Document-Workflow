ca52342dff441ca3ff3c5775eb34b22cd5af4e98
Mosa <letswalomosa@gmail.com>
2021-06-03 17:55:46 +0200

    Update document.service.spec.ts
    FIletransfer and download

--- app/src/app/Services/document.service.spec.ts
+++ app/src/app/Services/document.service.spec.ts
@@ -1,16 +1,32 @@
-import { TestBed } from '@angular/core/testing';
-
+import { File } from "@ionic-native/file/ngx";
+import { FileTransfer } from "@ionic-native/file-transfer/ngx";
+import { FileOpener } from "@ionic-native/file-opener/ngx";
+import { Injectable } from "@angular/core";
 import { DocumentService } from './document.service';
 
-describe('DocumentServiceService', () => {
-  let service: DocumentService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(DocumentService);
-  });
+@Injectable
+({
+  providedIn: "root"
+})
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+export class DocumentService {
+  fileTransfer: FileTransferObject;
+  constructor(
+    private fileOpener: FileOpener,
+    private transfer: FileTransfer,
+    private file: File
+  ) {}
+  download(url: string, title: string) {
+    this.fileTransfer = this.transfer.create();
+    this.fileTransfer
+      .download(url, this.file.dataDirectory + title + ".pdf")
+      .then(entry => {
+        console.log("downloaded successfully: " + entry.toURL());
+        this.fileOpener
+          .open(entry.toURL(), "application/pdf")
+          .then(() => console.log("File is opened"))
+          .catch(e => console.log("Error opening file", e));
+      });
+  }
+}
