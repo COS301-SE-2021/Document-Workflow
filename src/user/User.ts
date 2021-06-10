14753ae1afb81f01563074a845cf77dbe01a4876
delareytuks <u14011639@tuks.co.za>
2021-06-10 18:26:53 +0200

    temporarily commented out password hashing algorithm

--- src/user/User.ts
+++ src/user/User.ts
@@ -1,5 +1,5 @@
-const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 
 /**
@@ -9,13 +9,13 @@ const bcrypt = require("bcryptjs");
  * A password is valid iff it contains an uppercase,lowercase annd special character as well as being 8 characters long.
  * //TODO: add signature to this list.
  */
-const userSchema = mongoose.Schema({
-    name: { type: String, required: true },
-    surname: { type: String, required: true },
-    initials: { type: String, required: true},
+const userSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    surname: {type: String, required: true},
+    initials: {type: String, required: true},
     email: {
         type: String,
-        required: [ true, "Email is required" ],
+        required: [true, "Email is required"],
         trim: true,
         lowercase: true,
         unique: true,
@@ -25,7 +25,8 @@ const userSchema = mongoose.Schema({
             }
         }
     },
-    password: { type: String,
+    password: {
+        type: String,
         required: true,
         validate: {
             validator: value => {
@@ -34,16 +35,17 @@ const userSchema = mongoose.Schema({
         }
     },
     // signature: { type: String, required: true },
-    validated: { type: Boolean, default: false },
-    tokenDate: { type: Date, default: Date.now }
+    validated: {type: Boolean, default: false},
+    tokenDate: {type: Date, default: Date.now}
 });
 
 /**
  * This function is called automatically  before the save function is called is called for a user.
  * It handles the process of salting and hashing a user password and sets the user's password to the
  * generated hash.
+ * TODO: Figure out what happened after I changed require to import, why does usr.password fail now?
  */
-userSchema.pre("save", function(next)  {  //Cannot use lexical notation here (see: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions")
+/*userSchema.pre("save", function(next)  {  //Cannot use lexical notation here (see: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions")
     const usr = this;
     if(this.isModified("password") || this.isNew){
         bcrypt.genSalt(parseInt(process.env.SALT_ROUNDS), (saltErr,salt) => {
@@ -60,6 +62,6 @@ userSchema.pre("save", function(next)  {  //Cannot use lexical notation here (se
     } else {
         return next();
     }
-});
+});*/
 
 module.exports = mongoose.model('User', userSchema);
