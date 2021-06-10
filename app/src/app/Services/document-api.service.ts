4bca4aaef1d9064a5e76967e68bb69a989672a6e
u17112592 <u17112592@tuks.co.za>
2021-06-10 14:22:07 +0200

    Added a test function to aid with developing.

--- app/src/app/Services/document-api.service.ts
+++ app/src/app/Services/document-api.service.ts
@@ -24,4 +24,22 @@ constructor(private http:HttpClient) { };
     return this.http.get<documentImage[]>(`${this.url}/image`);
   }
 
+  public static async testUploadDocument(file: File): Promise<boolean>
+  {
+    const formData = new FormData();
+    formData.append('description', 'meow Meow Meow');
+    formData.append('document', file);
+    const response = await fetch(('http://localhost:3000/api/documents'), { //TODO: change this url
+      method: 'POST',
+      body: formData,
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      headers: {'Content-Type': 'multipart/form-data'}
+    });
+    //Content-Type: multipart/form-data
+    //'application/json; charset=UTF-8'
+
+    if(response.ok)
+    {return true;}
+    else {return false;}
+  }
 }
