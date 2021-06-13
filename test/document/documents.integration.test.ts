8e4430afe4b4ab462dbfab61a530f9aa5c1499d0
delareytuks <u14011639@tuks.co.za>
2021-06-13 23:29:32 +0200

    created new test files for integration tests

---
+++ test/document/documents.integration.test.ts
@@ -0,0 +1,7 @@
+import DocumentController from "../../src/document/DocumentController";
+import DocumentRepository from "../../src/document/DocumentRepository";
+import DocumentService from "../../src/document/DocumentService";
+
+describe("DocumentController: INTEGRATION TESTS", () => {
+
+});
