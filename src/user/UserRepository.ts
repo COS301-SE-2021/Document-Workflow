0d1016fe97e74b0365ff9169a5d55bc7ce9c0bf7
delareytuks <u14011639@tuks.co.za>
2021-06-13 23:34:50 +0200

    Implemented UserRepository.ts

---
+++ src/user/UserRepository.ts
@@ -0,0 +1,46 @@
+import User, { UserI } from "./User";
+
+export default class UserRepository {
+
+    async postUser(Usr: UserI): Promise<void> {
+        const usr = new User({
+            name: Usr.name,
+            surname: Usr.surname,
+            initials: Usr.initials,
+            email: Usr.email,
+            password: Usr.password,
+            validated: Usr.validated,
+            tokenDate: Usr.tokenDate
+        });
+        try{
+            await usr.save();
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    async getUsers(): Promise<UserI[]> {
+        try {
+            return await User.find({});
+        } catch(err) {
+            throw err;
+        }
+    }
+
+    async getUser(id: string) {
+        try {
+            return await User.findById(id);
+        } catch(err) {
+            throw err;
+        }
+    }
+
+    async deleteUser(id: string) {
+        try {
+            const usr: UserI = await this.getUser(id);
+            return await User.deleteOne(usr);
+        } catch(err) {
+            throw err;
+        }
+    }
+}
