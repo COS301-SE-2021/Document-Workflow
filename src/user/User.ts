18a119c929f877165ce0bed7beccc338d964f23f
delareytuks <u14011639@tuks.co.za>
2021-06-13 23:33:35 +0200

    added UserI interface to be used with User Schema added compare function to be used with login to compare passwords uncommented password hashing

--- src/user/User.ts
+++ src/user/User.ts
@@ -1,6 +1,15 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 
+export interface UserI {
+    name: string,
+    surname: string,
+    initials: string,
+    email: string,
+    password: string,
+    validated: Boolean,
+    tokenDate: Date
+}
 
 /**
  * The schema for a user. Since we are making use of NoSQL, this in essence defines the structure
@@ -9,7 +18,7 @@ import bcrypt from "bcryptjs";
  * A password is valid iff it contains an uppercase,lowercase annd special character as well as being 8 characters long.
  * //TODO: add signature to this list.
  */
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema<UserI>({
     name: {type: String, required: true},
     surname: {type: String, required: true},
     initials: {type: String, required: true},
@@ -45,7 +54,7 @@ const userSchema = new mongoose.Schema({
  * generated hash.
  * TODO: Figure out what happened after I changed require to import, why does usr.password fail now?
  */
-/*userSchema.pre("save", function(next)  {  //Cannot use lexical notation here (see: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions")
+userSchema.pre("save", function(next)  {
     const usr = this;
     if(this.isModified("password") || this.isNew){
         bcrypt.genSalt(parseInt(process.env.SALT_ROUNDS), (saltErr,salt) => {
@@ -62,6 +71,15 @@ const userSchema = new mongoose.Schema({
     } else {
         return next();
     }
-});*/
+});
 
-module.exports = mongoose.model('User', userSchema);
+export function compare(pass,hashed){
+    bcrypt.compare(pass, hashed, (err,match) => {
+        if(err){
+            throw err;
+        } else return match;
+    });
+    return false;
+}
+
+export default mongoose.model<UserI>('User', userSchema);
