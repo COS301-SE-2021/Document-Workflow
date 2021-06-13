c6f7d1233abb42e21950810627ba81733cfa2a76
delareytuks <u14011639@tuks.co.za>
2021-06-13 23:34:25 +0200

    implemented new dependency injected version of User Contoller

--- src/user/UserController.ts
+++ src/user/UserController.ts
@@ -1,108 +1,86 @@
-const express = require("express");
-const User = require("./User");
-const router = express.Router();
-const bcrypt = require("bcryptjs");
+import { Router } from "express";
+import { autoInjectable } from "tsyringe";
+import UserService from "./UserService";
+import { UserI } from "./User";
 
 // "/api/users"
 
-router.get('/:id', (req,res)=>{
-    User.findById(req.params.id)
-        .then((usr)=>{
-            if(usr){
-                res.status(200).json({
-                    message: "Success!",
-                    id: usr._id,
-                    name: usr.name,
-                    surname: usr.surname,
-                    email: usr.email
-                });
-            } else{
-                res.status(404).json({
-                    message: "User was not found"
-                });
-            }
-        })
-        .catch((msg)=>{
-            console.log(msg);
-            res.status(500).json({
-                message: msg
-            });
-        });
-});
+@autoInjectable()
+export default class UserController{
+    router: Router;
 
-function compare(pass,hashed){
-    bcrypt.compare(pass, hashed, (err,match) => {
-        if(err){
+    constructor(private userService: UserService) {
+        this.router = new Router();
+    }
+
+    async getUsersRoute(): Promise<UserI[]> {
+        try{
+            return await this.userService.getUsers();
+        } catch(err) {
             throw err;
-        } else return match;
-    });
-    return false;
-}
+        }
+    }
 
-router.post('/login/:id', (req, res) => {
-    //res.json(login_user.handle(req));
-    User.findById(req.params.id)
-        .then((usr)=>{
-            if(usr){
-                // if(compare(req.body.password, usr.password)){
-                if(req.body.password === usr.password){
-                    res.status(200).json({
-                        message: "Success!",
-                        token: "generated token"
-                    });
-                } else {
-                    res.status(401).json({
-                        message: "Unauthorized"
-                    });
-                }
-            } else{
-                res.status(404).json({
-                    message: "User was not found"
-                });
+    async getUserRoute(request): Promise<UserI> {
+        try{
+            return await this.userService.getUser(request);
+        }catch(err) {
+            throw err;
+        }
+    }
+
+    async postUserRoute(request) {
+        try {
+            return await this.userService.postUser(request);
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    /*
+    * error codes:
+    * 200 OK request succeeded
+    * 201 Created
+    * 202 Accepted -> received but not acted upon
+    * 400 Bad request
+    * 401 Unauthorized -> (unauthenticated) Client id not known
+    * 403 Forbidden -> Client id known
+    * 404 Not found
+    * 405 Method not allowed
+    * 418 I'm a teapot
+    * 500 Internal Server Error
+    * 507 Insufficient Storage
+    * */
+
+    routes() {
+        this.router.get("", async (req, res) => {
+            try {
+                res.status(200).json(await this.getUsersRoute());
+            } catch(err){
+                res.status(400).json(err);
             }
-        })
-        .catch((msg)=>{
-            console.log(msg);
-            res.status(500).json({
-                message: msg
-            });
         });
-});
 
-/**
- * The api entry point for registering a new user. The request requires the following body parameters be set:
- *  name: the user's firstname
- *  surname: the user's surname
- *  initials the user's initials
- *  email: the user's email address
- *  password: the user's password
- *
- *  TODO: incorporate the signature to this function.
- *  TODO: abstract the database functionality to a different file.
- */
-router.post('', (req, res) => {
+        this.router.get("/:id", async (req, res) => {
+            try {
+                res.status(200).json(await this.getUserRoute(req));
+            } catch(err){
+                if(err instanceof URIError){
+                    res.status(400).json("id field is required");
+                } else {
+                    res.status(400).json(err);
+                }
 
-    //TODO: Convert password to hash with bcryptjs
-    const user = new User({
-        name: req.body.name,
-        surname: req.body.surname,
-        initials: req.body.initials,
-        email: req.body.email,
-        password: req.body.password,
-    });
-
-    user.save()
-        .then((usr)=>{
-            res.status(200).json({
-                message: "User added successfully",
-                userId: usr._id
-            });
-        })
-        .catch((msg)=>{
-            res.status(500).json({
-                message: msg
-            });
+            }
         });
-});
 
-module.exports = router;
+        this.router.post("", async (req,res) => {
+            try {
+                res.status(201).json(await this.postUserRoute(req));
+            } catch(err){
+                res.status(400).json(err);
+            }
+        });
+        return this.router;
+    }
+}
