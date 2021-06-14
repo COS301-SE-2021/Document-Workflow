6d638b34c4e6c2afd9143b9ce2f38038ad0fe752
u17112592 <u17112592@tuks.co.za>
2021-06-14 18:30:11 +0200

    Added the postWorkflow functionality so that the details of a workFlow can be stored.

--- src/workflow/WorkFlowRepository.ts
+++ src/workflow/WorkFlowRepository.ts
@@ -1,7 +1,24 @@
-import WorkFlow, {WorkflowI} from "./WorkFlow";
+import WorkFlow, {WorkFlowI} from "./WorkFlow";
 
 export default class WorkFlowRepository{
 
+    async postWorkFlow(workflow: WorkFlowI) : Promise<void> {
+
+        const new_workflow = new WorkFlow({
+            name: workflow.name,
+            owner_id: workflow.owner_id,
+            owner_email: workflow.owner_email,
+            document_id: workflow.document_id,
+            document_path: workflow.document_path,
+            members: workflow.members
+
+        });
+        try {
+            await new_workflow.save();
+        } catch (err) {
+            throw err;
+        }
+    }
 
 
 
