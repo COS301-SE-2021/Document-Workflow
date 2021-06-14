3c794a34e1eb40cdeba23779e268295e961cdd99
u17112592 <u17112592@tuks.co.za>
2021-06-14 18:30:42 +0200

    Added the service for creating a new workflow.

--- src/workflow/WorkFlowService.ts
+++ src/workflow/WorkFlowService.ts
@@ -1,5 +1,5 @@
 import { injectable } from "tsyringe";
-import {WorkflowI} from './WorkFlow';
+import {WorkFlowI} from './WorkFlow';
 import WorkFlowRepository from './WorkFlowRepository';
 
 @injectable()
@@ -8,5 +8,26 @@ export default class WorkFlowService{
     constructor(private workflowRepository: WorkFlowRepository)
     {
 
+    }
+
+    async createWorkFlow(req) :Promise<any>{
+        console.log(req);
+        //TODO: upload the file first or create the workflow first?
+        try{
+            const workflow : WorkFlowI = {
+                _id: null,
+                owner_id: null,
+                name: req.body.name,
+                owner_email: req.body.owner_email,
+                document_id: "60b89ade8d0127f52f8fa6cd", //TODO: upload the document and get its name
+                document_path: req.files.document.name, //TODO: set the document path to begin with this workflow's ID
+                members: req.body.members
+            }
+            await this.workflowRepository.postWorkFlow(workflow);
+            return "New workflow successfully created";
+        }
+        catch(err) {
+            throw err;
+        }
     }
 }
