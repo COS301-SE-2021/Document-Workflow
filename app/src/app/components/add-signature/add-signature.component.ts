c3912c9e056719ac316da22fb6f341b561249a40
Mosa <letswalomosa@gmail.com>
2021-06-16 07:23:46 +0200

    add-signature

---
+++ app/src/app/components/add-signature/add-signature.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-signature',
+  templateUrl: './add-signature.component.html',
+  styleUrls: ['./add-signature.component.scss'],
+})
+export class AddSignatureComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
