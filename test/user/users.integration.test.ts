cccae4efce8eb409d653efd21ce596f9adbd48e7
delareytuks <u14011639@tuks.co.za>
2021-06-18 10:06:02 +0200

    changed the purpose of the first test to test one complete User life cycle added code to detect and delete test user before test is run so that test can run again without manual deletion from database

--- test/user/users.integration.test.ts
+++ test/user/users.integration.test.ts
@@ -2,51 +2,67 @@ import UserService from "../../src/user/UserService";
 import UserRepository from "../../src/user/UserRepository";
 import UserController from "../../src/user/UserController";
 import Database from "../../src/Database";
+import Authentication from "../../src/auth/Authentication";
+import * as supertest from "supertest";
 
 let userService;
 let userController;
-describe("user integration tests", () => {
-  // Database.get();
+describe("User integration tests", () => {
+
   beforeAll(async () => {
       await Database.get();
   });
+
   beforeEach(() => {
     userService = new UserService(new UserRepository());
-    userController = new UserController(userService);
+    userController = new UserController(userService, new Authentication(userService));
   });
 
   afterAll(async () => {
     await Database.disconnect();
   });
 
-  describe("/POST", () => {
-    describe("/api/users", () => {
+  describe("User Lifecycle:", () => {
+
       test("Create new user:", async () => {
 
-        const mockGetRequest = {
+        const findByEmailRequest = {
           params: {
-           // id: postResponse._id,
+            email: "testymctestface@testmail.test"
           },
         } as unknown as Request;
 
-        //const getResponse = await userController.getUserRoute(mockGetRequest);
+        const getResponse = await userController.getUserByEmailRoute(findByEmailRequest);
+        if(getResponse){
+          //delete user
+          const deleteRequest = {
+            params: {
+              id: getResponse._id
+            }
+          } as unknown as Request;
+
+          await userController.deleteUserRoute(deleteRequest);
+
+        }
+
         const currentDate = Date.now();
         const mockPostRequest = {
           body: {
-            name: "Joey2",
-            surname: "Cooper2",
-            initials: "JC",
-            email: "joey@hotmail.gov",
+            name: "Testy",
+            surname: "Testson",
+            initials: "TT",
+            email: "testymctestface@testmail.test",
             password: "paSsW*or^d1234",
             validated: false,
             tokenDate: currentDate,
+            signature: ""
           },
         } as unknown as Request;
 
-        //const postResponse = await userController.postUserRoute(mockPostRequest);
+        const postResponse = await userController.registerUserRoute(mockPostRequest);
+        console.log(postResponse);
 
 
       });
     });
-  });
 });
