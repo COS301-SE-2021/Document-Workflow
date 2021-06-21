a5b457932f36044f40044760dc98dcd0a32aa229
u17015741 <54096396+u17015741@users.noreply.github.com>
2021-06-21 12:34:10 +0200

    tests

--- cypress/integration/Tests/test_test_spec.js
+++ cypress/integration/Tests/test_test_spec.js
@@ -52,15 +52,15 @@ describe('Logging in',()=>{
     it('tries to log in with incorrect email', ()=>{
       cy.get('#loginEmail').type('brentmweb.com')
       cy.get('#loginPassword').type('Password#1')
-      // cy.get('#login').should('#login.ion-color.ion-color-primary.md.button.button-block.button-outline.button-disabled.ion-activatable.ion-focusable.hydrated')
-      // cy.get('loginForm.valid').should('eq','false')
+      cy.get('#login').click({force: true})
+      cy.url().should('eq','http://localhost:8100/login')
     })
 
     it('Tries to log in with a to small password( char < 9)',()=>{
       cy.get('#loginEmail').type('brenton.stroberg@yahoo.co.za')
-      cy.get('#loginPassword').type('Password')
-      !cy.get('#login').click()
-      // cy.url().should('eq','')
+      cy.get('#loginPassword').type('Passwor')
+      cy.get('#login').click({force: true})
+      cy.url().should('eq','http://localhost:8100/login')
     })
 
     it('logs in with the correct email and password', ()=>{
@@ -78,28 +78,51 @@ describe('Navigation when user is logged in', ()=>{
     cy.get('#loginEmail').type('brenton.stroberg@yahoo.co.za')
     cy.get('#loginPassword').type('Password#1')
     cy.get('#login').click()
+    cy.get('body').click(0,0);
     cy.wait(1000)
+    cy.get('body').click(0,0);
+    cy.get('body').click(0,0);
 })
 
-  it('navigating to profile page', () =>{
+  it('Navigating to profile page', () =>{
     cy.visit('http://localhost:8100/userProfile')
     cy.url().should('eq','http://localhost:8100/userProfile')
   })
 
-  it('navigating to archive', () =>{
+  it('Navigating to archive', () =>{
     cy.visit('http://localhost:8100/archive')
     cy.url().should('eq','http://localhost:8100/archive')
   })
 
-  it('navigating to view document', () =>{
+  it('Navigating to view document', () =>{
     cy.visit('http://localhost:8100/documentView')
     cy.url().should('eq','http://localhost:8100/documentView')
   })
 
-  it('navigating to home', () =>{
+  it('Navigating to home', () =>{
     cy.visit('http://localhost:8100/home')
     cy.url().should('eq','http://localhost:8100/home')
   })
 })
 
+describe('Testing the loading workflow', ()=>{
+  beforeEach(()=>{
+    cy.visit('http://localhost:8100')
+    cy.get('#loginEmail').type('brenton.stroberg@yahoo.co.za')
+    cy.get('#loginPassword').type('Password#1')
+    cy.get('#login').click()
+    cy.wait(1500)
+    cy.get('body').click(0,0);
+    cy.get('body').click(0,0);
+    cy.get('body').click(0,0);
+  })
+
+  it('Getting the form name',()=>{
+    cy.get('ion-card-title').contains( 'testing document')
+  })
+
+  it('Getting owner', ()=>{
+    cy.get('ion-card-subtitle').contains('Owner of workflow: brenton.stroberg@yahoo.co.za ')
+  })
+})
   
