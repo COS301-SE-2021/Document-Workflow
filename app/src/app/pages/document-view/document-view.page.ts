a9abdc7cb1497bbdbc42928622a78f862861ae7c
u17015741 <54096396+u17015741@users.noreply.github.com>
2021-07-01 23:22:36 +0200

    x stays constant even after changing screen size and zooming in and out

--- app/src/app/pages/document-view/document-view.page.ts
+++ app/src/app/pages/document-view/document-view.page.ts
@@ -4,7 +4,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Filesystem, Directory, Encoding } from '@capacitor/filesystem';
 
-
 import { ModalController, NavParams, Platform } from '@ionic/angular';
 import { HttpClient } from '@angular/common/http';
 import { AddSignatureComponent } from 'src/app/components/add-signature/add-signature.component';
@@ -13,7 +12,7 @@ import { DocumentAPIService } from 'src/app/Services/Document/document-api.servi
 import { async } from '@angular/core/testing';
 import { ConfirmSignaturesComponent } from 'src/app/components/confirm-signatures/confirm-signatures.component';
 import { degrees, PDFDocument, rgb, StandardFonts } from 'pdf-lib';
-import {DomSanitizer} from "@angular/platform-browser";
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-document-view',
@@ -26,7 +25,6 @@ export class DocumentViewPage implements OnInit {
   setZoom: any;
   zoomLevel: number;
   pdfDoc: PDFDocument;
-
 
   @Input('id') id: string;
   @Input('documentname') docName: string;
@@ -41,18 +39,17 @@ export class DocumentViewPage implements OnInit {
   async ngOnInit() {
     this.rotated = 0;
     this.setZoom = 'false';
-    this.zoomLevel=1;
-    await this.route.params.subscribe(stuff =>{
+    this.zoomLevel = 1;
+    await this.route.params.subscribe((stuff) => {
       this.id = stuff['id'];
       this.docName = stuff['documentname'];
     });
-    await (this.getDocument(this.id));
+    await this.getDocument(this.id);
   }
 
   download() {
-    const blob = new Blob([this.srcFile], {type: 'application/pdf'});
+    const blob = new Blob([this.srcFile], { type: 'application/pdf' });
     const objUrl = URL.createObjectURL(blob);
-
 
     var link = document.createElement('a');
     link.href = objUrl;
@@ -63,54 +60,53 @@ export class DocumentViewPage implements OnInit {
     link.remove();
   }
 
-  back(){
+  back() {
     this.router.navigate(['home']);
   }
 
-
-  async getDocument(id: string){
-    this.docApi.getDocument(id, async (response)=>{
-      if (response){
+  async getDocument(id: string) {
+    this.docApi.getDocument(id, async (response) => {
+      if (response) {
         const buff = response.data.filedata.Body.data; //wut
-        const a  = new Uint8Array( buff);
+        const a = new Uint8Array(buff);
 
         this.pdfDoc = await PDFDocument.load(a);
         const pdfBytes = await this.pdfDoc.save();
         this.srcFile = pdfBytes;
-      }else{
-
+      } else {
       }
     });
   }
 
-  async printBrentIdea(event){
-
+  async printBrentIdea(event) {
     let xCanvas = document.getElementsByTagName('canvas')[0].style.width;
-    let x = parseInt(xCanvas.substring(0, xCanvas.length-2));
+    let x = parseInt(xCanvas.substring(0, xCanvas.length - 2));
     let yCanvas = document.getElementsByTagName('canvas')[0].style.height;
-    let y = parseInt(yCanvas.substring(0, yCanvas.length-2));
+    let y = parseInt(yCanvas.substring(0, yCanvas.length - 2));
     //todo check boundaries
+    let widthOfScreen =
+      document.getElementsByTagName('canvas')[0].parentElement.parentElement
+        .parentElement.parentElement.clientWidth;
+    let widthOffset = (widthOfScreen - x) / 2;
 
-    let yCoord = y + (event.clientY - y ) ;
-    let xCoord = x + (event.clientX - x ) ;
-    console.log('Size:\t X: '+ xCanvas + '\tY:' + yCanvas)
-    console.log('Client:\t X:' +event.clientX +'\tY:'+ event.clientY)
+    console.log('full width: ' + widthOfScreen);
+    console.log('offset width: ' + widthOffset);
 
     let pdfBytes = await this.pdfDoc.save();
     this.pdfDoc = await PDFDocument.load(pdfBytes);
     const pages = this.pdfDoc.getPages();
     const firstPage = pages[0];
     const { width, height } = firstPage.getSize();
-    console.log(width, ' ', height)
+    console.log(width + ' ' + height);
     const helveticaFont = await this.pdfDoc.embedFont(StandardFonts.Helvetica);
 
+    let ratio =0
+    if (widthOffset > 1) {
+      ratio = (widthOffset/x)* width;
+    }
 
-
-    xCoord =((event.clientX * width)/x);
-    yCoord = height - ((event.clientY * height)/y);
-
-    console.log('X: '+xCoord +'\tY: '+yCoord)
-
+    let xCoord = (event.clientX * width) / x - ratio;
+    let yCoord = ((y - event.clientY) / y) * height + 30 / this.zoomLevel;
 
     firstPage.drawText('X', {
       x: xCoord,
@@ -124,8 +120,8 @@ export class DocumentViewPage implements OnInit {
     this.srcFile = pdfBytes;
   }
 
-  async printMousePosition(event){
-    console.log("X: ", event.clientX, " Y: ", event.clientY);
+  async printMousePosition(event) {
+    console.log('X: ', event.clientX, ' Y: ', event.clientY);
     let pdfBytes = await this.pdfDoc.save();
     this.pdfDoc = await PDFDocument.load(pdfBytes);
     const pages = this.pdfDoc.getPages();
@@ -145,52 +141,52 @@ export class DocumentViewPage implements OnInit {
     this.srcFile = pdfBytes;
   }
 
-  async sign(){
+  async sign() {
     const sign = this.modalCtrl.create({
-      component: ConfirmSignaturesComponent
+      component: ConfirmSignaturesComponent,
     });
 
     (await sign).present();
 
-    const data =  (await sign).onDidDismiss();
-    if(await (await data).data['confirm']){
+    const data = (await sign).onDidDismiss();
+    if (await (await data).data['confirm']) {
       this.addSignature();
     }
   }
 
-  rotation(){
+  rotation() {
     this.rotated += 90;
-    if(this.rotated === 360){
-      this.rotated =0;
+    if (this.rotated === 360) {
+      this.rotated = 0;
     }
   }
 
-  setZoomWidth(){
-    this.setZoom ='page-width';
+  setZoomWidth() {
+    this.setZoom = 'page-width';
     console.log('width');
   }
 
-  setZoomFit(){
-    this.setZoom ='page-fit';
+  setZoomFit() {
+    this.setZoom = 'page-fit';
     console.log('fit');
   }
 
-  setZoomHeight(){
-    this.setZoom ='page-height';
+  setZoomHeight() {
+    this.setZoom = 'page-height';
     console.log('height');
   }
 
-  zoomIn(){
+  zoomIn() {
     this.zoomLevel += 0.25;
     console.log('in');
   }
 
-  zoomOut(){
-    this.zoomLevel -=0.25;
+  zoomOut() {
+    this.zoomLevel -= 0.25;
     console.log('out');
   }
 
-  addSignature(){
+  addSignature() {
     console.log('here');
   }
 }
