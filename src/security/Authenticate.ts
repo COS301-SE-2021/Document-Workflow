d2614a804f3fa20ca874f0011c9e3a27372ca23e
delareytuks <u14011639@tuks.co.za>
2021-07-15 09:22:37 +0200

    Now throws AuthenticationError, no longer catches error itself

--- src/security/Authenticate.ts
+++ src/security/Authenticate.ts
@@ -1,25 +1,22 @@
 import { autoInjectable } from "tsyringe";
-import UserService from "./user/UserService";
+import UserService from "../user/UserService";
 import jwt from "jsonwebtoken";
-@autoInjectable()
+import {AuthenticationError} from "../error/Error";
 
+@autoInjectable()
 export default class Authenticator {
 
     constructor(private userService: UserService) {
     }
 
     Authenticate = async (req, res, next) => {
-        try {
             const token = req.header("Authorization").replace("Bearer ", "");
             const decoded = jwt.verify(token, process.env.SECRET);
             const user = await this.userService.getUser({_id: decoded._id, 'tokens.token': token});
             if (!user) {
-                throw new Error("User could not be found");
+                throw new AuthenticationError("User could not be found");
             }
             req.user = user;
             next();
-        } catch (e) {
-            res.status(401).send({status: "failed", data: {}, message: "Unable to Authenticate"});
-        }
     }
 }
