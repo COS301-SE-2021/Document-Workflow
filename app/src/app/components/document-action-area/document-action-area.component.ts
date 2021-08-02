4ee9c8d6886021a5faa7783dfd642db1b2c10689
u17112592 <u17112592@tuks.co.za>
2021-08-02 15:07:03 +0200

    Added a nice svg for toggling annotations.

--- app/src/app/components/document-action-area/document-action-area.component.ts
+++ app/src/app/components/document-action-area/document-action-area.component.ts
@@ -64,10 +64,11 @@ export class DocumentActionAreaComponent implements OnInit, AfterViewInit {
             header.getHeader('toolbarGroup-Annotate').delete('strikeoutToolGroupButton');
             header.getHeader('toolbarGroup-Annotate').delete('squigglyToolGroupButton');
             header.getHeader('toolbarGroup-Annotate').delete('freeTextToolGroupButton');
+            header.getHeader('toolbarGroup-Annotate').delete('freeHandToolGroupButton');
 
             header.push({
               type: 'actionButton',
-              img: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M17 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V7l-4-4zm-5 16c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3zm3-10H5V5h10v4z"/></svg>',
+              img: '<svg xmlns=\'http://www.w3.org/2000/svg\' class=\'ionicon\' viewBox=\'0 0 512 512\'><title>Eye</title><path d=\'M255.66 112c-77.94 0-157.89 45.11-220.83 135.33a16 16 0 00-.27 17.77C82.92 340.8 161.8 400 255.66 400c92.84 0 173.34-59.38 221.79-135.25a16.14 16.14 0 000-17.47C428.89 172.28 347.8 112 255.66 112z\' fill=\'none\' stroke=\'currentColor\' stroke-linecap=\'round\' stroke-linejoin=\'round\' stroke-width=\'32\'/><circle cx=\'256\' cy=\'256\' r=\'80\' fill=\'none\' stroke=\'currentColor\' stroke-miterlimit=\'10\' stroke-width=\'32\'/></svg>',
               onClick: () =>  { this.toggleAnnotations(annotManager);
               }
             });
