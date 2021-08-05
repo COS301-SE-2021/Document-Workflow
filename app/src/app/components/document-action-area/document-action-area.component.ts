19dd121badbddc00dce0c42c1ee5a5532e9e6831
u17112592 <u17112592@tuks.co.za>
2021-08-05 08:58:14 +0200

    Finalized adding action-area UI.

--- app/src/app/components/document-action-area/document-action-area.component.ts
+++ app/src/app/components/document-action-area/document-action-area.component.ts
@@ -28,11 +28,6 @@ export class DocumentActionAreaComponent implements OnInit, AfterViewInit {
   @Input('ownerEmail') ownerEmail: any;
   constructor(
     private modal: ModalController,
-    private navpar: NavParams,
-    private route: ActivatedRoute,
-    private docApi: DocumentAPIService,
-    private workFlowService: WorkFlowService,
-    private router: Router,
   ) {}
 
   async ngOnInit() {
@@ -53,6 +48,10 @@ export class DocumentActionAreaComponent implements OnInit, AfterViewInit {
           //We only want to display the Annotations ribbon
           instance.UI.disableElements(['ribbons']);
           instance.UI.setToolbarGroup('toolbarGroup-Annotate',false);
+
+
+
+          //adds the button used to toggle visibility of annotations
           instance.UI.setHeaderItems(header =>{
             header.push({
               type: 'actionButton',
@@ -60,16 +59,13 @@ export class DocumentActionAreaComponent implements OnInit, AfterViewInit {
               onClick: () =>  { this.toggleAnnotations(annotationManager);
               }
             });
+            //Disable annotation tools that will not be supported.
+            header.getHeader('toolbarGroup-Annotate').delete('freeTextToolGroupButton');
+            header.getHeader('toolbarGroup-Annotate').delete('freeHandToolGroupButton');
+            header.getHeader('toolbarGroup-Annotate').delete('freeHandHighlightToolGroupButton');
           });
 
           /*
-            header.push({
-              type: 'actionButton',
-              img: '<svg xmlns=\'http://www.w3.org/2000/svg\' class=\'ionicon\' viewBox=\'0 0 512 512\'><title>Eye</title><path d=\'M255.66 112c-77.94 0-157.89 45.11-220.83 135.33a16 16 0 00-.27 17.77C82.92 340.8 161.8 400 255.66 400c92.84 0 173.34-59.38 221.79-135.25a16.14 16.14 0 000-17.47C428.89 172.28 347.8 112 255.66 112z\' fill=\'none\' stroke=\'currentColor\' stroke-linecap=\'round\' stroke-linejoin=\'round\' stroke-width=\'32\'/><circle cx=\'256\' cy=\'256\' r=\'80\' fill=\'none\' stroke=\'currentColor\' stroke-miterlimit=\'10\' stroke-width=\'32\'/></svg>',
-              onClick: () =>  { this.toggleAnnotations(annotManager);
-              }
-            });
-
             header.push({
               type: 'actionButton',
               img: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M17 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V7l-4-4zm-5 16c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3zm3-10H5V5h10v4z"/></svg>',
@@ -80,9 +76,7 @@ export class DocumentActionAreaComponent implements OnInit, AfterViewInit {
             });
           });
            */
-
         });
-
   }
 
   toggleAnnotations(annotationManager){
@@ -107,7 +101,7 @@ export class DocumentActionAreaComponent implements OnInit, AfterViewInit {
   //TODO: this xfdfString should be reloaded back into this phase if the user decides to add more action areas.
   //This will ensure that they dont have to redo all the work that they did earlier.
   async back() {
-    this.xfdfString = this.annotationManager.getAnnotCommand();
+    this.xfdfString = await this.annotationManager.exportAnnotations({links:false, widgets: false});
     const mod = this.modal.create({
       component: ConfirmDeleteWorkflowComponent,
       componentProps:{
@@ -119,7 +113,7 @@ export class DocumentActionAreaComponent implements OnInit, AfterViewInit {
     (await mod).onDidDismiss().then(async (data) => {
       const result = (await data).data['confirm'];
       if (result){
-        this.modal.dismiss({
+        await this.modal.dismiss({
           'xfdfString': this.xfdfString,
         })
       }else{
