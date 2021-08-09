fe165f8120d68fef7711b1a8001b911f0bee182f
delareytuks <u14011639@tuks.co.za>
2021-08-10 00:29:36 +0200

    Phase schema has been updated to reflect newest revision

--- src/phase/Phase.ts
+++ src/phase/Phase.ts
@@ -1,32 +1,34 @@
-import { createSchema, ExtractDoc, Type, typedModel } from "ts-mongoose";
+import { createSchema, ExtractDoc, ExtractProps, Type, typedModel } from "ts-mongoose";
 import { userSchema } from "../user/User";
 
 export const ActionAreaType = ["Date","Signature", "initial"];
 export const PhaseStatus = ["Pending", "InProgress", "Rejected", "Completed"];
 
-const actionAreaSchema = createSchema({
+const actionAreaSchema = createSchema({ //sort of scrapped
     coordinates: Type.array({maxlength: 2, minlength: 2, required: true}).of(Type.number),
     type: Type.string({enum: ActionAreaType, required: true}),
     dimensions: Type.array({maxlength: 2, minlength: 2, required: true}).of(Type.number),
     content: Type.mixed()
 }, { _id: false, _v: false });
 
-const commentSchema = createSchema({
+const commentSchema = createSchema({ //sort of scrapped
     userId: Type.ref(Type.objectId({required: true})).to("User", userSchema),
     content: Type.string({required: true})
 }, { _id: false, _v: false });
 
 export const phaseSchema = createSchema({
     users: Type.array({required: true}).of(Type.ref(Type.objectId()).to("User", userSchema)),
-    comments: Type.array().of(commentSchema),
+    //comments: Type.array().of(commentSchema),
+    annotations: Type.string({required: true}),
     description: Type.string({required: true}),
-    actionAreas: Type.array().of(actionAreaSchema),
+    //actionAreas: Type.array().of(actionAreaSchema), //annotations: string -> includes comments
     signingUserId: Type.ref(Type.objectId({required:true})).to("User", userSchema),
-    status: Type.string({enum: PhaseStatus, required: true})
-}, { _id: false, _v: false });
+    //status: Type.string({enum: PhaseStatus, required: true}),
+    userAccepts: Type.array().of(Type.array({maxlength: 2, minlength: 2, required: false}).of(Type.string()))
+}, { _id: true, _v: false });
 
 export const Phase = typedModel('Phase', phaseSchema);
-export type PhaseDoc = ExtractDoc<typeof phaseSchema>;
+export type PhaseProps = ExtractProps<typeof phaseSchema>;
 
 export const ActionArea = typedModel('ActionArea', actionAreaSchema);
-export type ActionAreaDoc = ExtractDoc<typeof actionAreaSchema>;
+export type ActionAreaProps = ExtractProps<typeof actionAreaSchema>;
