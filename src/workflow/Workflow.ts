e42c47275b52b8a85c778b6b5f93f2fe1807a4d6
delareytuks <u14011639@tuks.co.za>
2021-08-10 00:31:18 +0200

    Renamed model and props to be consistent, changed phases property to be an array of references to Phase entities

---
+++ src/workflow/Workflow.ts
@@ -0,0 +1,20 @@
+import {
+    createSchema,
+    Type,
+    typedModel,
+    ExtractDoc, ExtractProps
+} from "ts-mongoose";
+import { phaseSchema } from "../phase/Phase";
+import { userSchema } from "../user/User";
+import { documentSchema } from "../document/Document";
+
+export const workflowSchema = createSchema({
+    name: Type.string({required: true}),
+    ownerId: Type.ref(Type.objectId({required: true})).to("User", userSchema),
+    documentId: Type.ref(Type.objectId({required: false})).to("Document", documentSchema),
+    description: Type.string({required: true}),
+    phases: Type.array({required: false}).of(Type.ref(Type.objectId({required: true})).to("Phase", phaseSchema))
+}, {_id: true, _v: false});
+
+export const Workflow = typedModel('WorkFlowModel', workflowSchema);
+export type WorkflowProps = ExtractProps<typeof workflowSchema>;
