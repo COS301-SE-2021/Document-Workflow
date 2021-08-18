6026a512f0dbe62ac41e2b13795229b11c448605
delareytuks <u14011639@tuks.co.za>
2021-08-18 19:35:33 +0200

    fixed beforeEach section to reflect new dependencies, added new unit test skeletons

--- test/user/users.integration.test.ts
+++ test/user/users.integration.test.ts
@@ -3,6 +3,7 @@ import UserRepository from "../../src/user/UserRepository";
 import UserController from "../../src/user/UserController";
 import Database from "../../src/Database";
 import WorkFlowRepository from "../../src/workflow/WorkflowRepository";
+import Authenticator from "../../src/security/Authenticate";
 
 let userService;
 let userController;
@@ -14,7 +15,7 @@ describe("User integration tests", () => {
 
   beforeEach(() => {
     userService = new UserService(new UserRepository(), new WorkFlowRepository());
-    userController = new UserController(userService);
+    userController = new UserController(userService, new Authenticator(userService));
   });
 
   afterAll(async () => {
@@ -23,8 +24,10 @@ describe("User integration tests", () => {
 
   describe("User Lifecycle:", () => {
 
-      test("Create new user:", async () => {
+      it("Should create a new user:", async () => {
 
+        //Create new user if it doesn't exist
+        //Check if user exists
         const findByEmailRequest = {
           params: {
             email: "testymctestface@testmail.test"
@@ -63,5 +66,31 @@ describe("User integration tests", () => {
         await userController.registerUserRoute(mockPostRequest);
         //console.log(postResponse);
       });
+
+      it("Should delete a user", () => {
+
+      });
+
+      it("Should edit a user", () => {
+
+      });
+
+      it("Should add friends to a user", () => {
+
+      });
+
+      it("Should remove a friend from a user", () => {
+
+      });
+
+
+      //Negative tests
+      it("Should throw Authentication Error", () => {
+
+      });
+
+
+
+
     });
 });
