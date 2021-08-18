db78c9070223f6bf5a68d377ab7696edc1d8b551
u17015741 <54096396+u17015741@users.noreply.github.com>
2021-08-18 19:35:12 +0200

    Testing for workflow and add workflow

--- cypress/integration/Tests/Workflow/Workflow.spec.js
+++ cypress/integration/Tests/Workflow/Workflow.spec.js
@@ -9,9 +9,21 @@ describe("testing the home workflow without logging in", () => {
         cy.get('#loginPassword').type('Password#1')
         cy.get('#login').click()
         cy.url().should('eq','http://localhost:8100/home');
+        cy.wait(8000);
+        cy.get('ion-popover').click();
     });
 
-    it('',()=>{
+    it('check is workflow exists',()=>{
+      cy.get('ion-card-title').eq(4).should('contain', 'Brents Workflow for Testing cypress');
+    });
 
-    })
+    it('checks if owner is correct', ()=>{
+      cy.get('ion-card-subtitle').eq(4).should('contain', 'Owner of the workflow: brenton.stroberg@yahoo.co.za');
+    });
+
+    it('checks for the correct number of phases', ()=>{
+      cy.get('ion-card').eq(4).get('h1').eq(11).should('contain', 'Phase 3')
+    });
+
+    
 });
