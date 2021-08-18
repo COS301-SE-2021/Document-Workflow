1bf3f5287b4abed37d391ce5fe6be443f09b36f4
delareytuks <u14011639@tuks.co.za>
2021-08-18 19:37:10 +0200

    added necessary variables to simulate any dependency

--- test/workflow/workflow.test.ts
+++ test/workflow/workflow.test.ts
@@ -1,28 +1,67 @@
-import WorkFlowRepository from "../../src/workflow/WorkflowRepository";
-import WorkFlowService from "../../src/workflow/WorkflowService";
+import WorkflowRepository from "../../src/workflow/WorkflowRepository";
+import WorkflowService from "../../src/workflow/WorkflowService";
 import DocumentService from "../../src/document/DocumentService";
 import UserRepository from "../../src/user/UserRepository";
 import DocumentRepository from "../../src/document/DocumentRepository";
 import WorkflowController from "../../src/workflow/WorkflowController";
+import UserService from "../../src/user/UserService";
+import { PhaseRepository } from "../../src/phase/PhaseRepository";
+import Authenticator from "../../src/security/Authenticate";
 
 describe("unit tests for workflow", () => {
     let workflowRepository;
+    let userRepository;
+    let documentRepository;
+    let phaseRepository;
+
     let workflowService;
-    let workflowController;
+    let userService;
     let documentService;
+    let phaseService;
+
+    let authenticationService;
+
+    let workflowController;
 
     beforeAll(() => {
 
     })
 
     beforeEach(()=> {
-        /*documentService = new DocumentService(new DocumentRepository());
-        workflowRepository = new WorkFlowRepository();
-        workflowService = new WorkFlowService(workflowRepository, documentService, new UserRepository());
-        workflowController = new WorkflowController(workflowService);*/
+        workflowRepository = new WorkflowRepository();
+        userRepository = new UserRepository();
+        documentRepository = new DocumentRepository();
+        phaseRepository = new PhaseRepository();
+
+        userService = new UserService(userRepository, workflowRepository);
+        documentService = new DocumentService(documentRepository);
+        authenticationService = new Authenticator(userService);
+        workflowService = new WorkflowService(workflowRepository, documentService, userService, phaseService);
+
+        workflowController = new WorkflowController(workflowService, authenticationService);
     })
 
-    it("should", () => {
+    it("should verify that a user is verified", () => {
+
+    })
+
+    it("", () => {
+
+    })
+
+    it("", () => {
+
+    })
+
+    it("", () => {
+
+    })
+
+    it("", () => {
+
+    })
+
+    it("", () => {
 
     })
 })
