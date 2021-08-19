90408e415e324d9343d1f085757355e87fd8a136
u17015741 <54096396+u17015741@users.noreply.github.com>
2021-08-19 15:11:30 +0200

    Revert "Update intro.page.ts"
    This reverts commit d5c56eef0d9504d2ae71c15a09cce2ee8827255f.

--- app/src/app/pages/intro/intro.page.ts
+++ app/src/app/pages/intro/intro.page.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
-import { UserAPIService } from 'src/app/Services/User/user-api.service';
+
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.page.html',
@@ -8,21 +7,9 @@ import { UserAPIService } from 'src/app/Services/User/user-api.service';
 })
 export class IntroPage implements OnInit {
 
-
-  constructor(private router: Router, private userAPiService: UserAPIService) { }
+  constructor() { }
 
   ngOnInit() {
-  }
-  getStarted()
-  {
-    this.router.navigate(['login']);
-  }
-  // var dateToday = new Date();
-  // page.currentYear = dateToday.getFullYear();
-  async displaypopOver(even: Event )
-  {
-
-    this.userAPiService.displayPopOver('termsOfService','');
   }
 
 }
