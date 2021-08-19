d5c56eef0d9504d2ae71c15a09cce2ee8827255f
Mosa Letswalo <letswalomosa@gmail.com>
2021-08-19 14:53:24 +0200

    Update intro.page.ts
    routingNav && popOver

--- app/src/app/pages/intro/intro.page.ts
+++ app/src/app/pages/intro/intro.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Router} from '@angular/router';
+import { UserAPIService } from 'src/app/Services/User/user-api.service';
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.page.html',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IntroPage implements OnInit {
 
-  constructor() { }
+
+  constructor(private router: Router, private userAPiService: UserAPIService) { }
 
   ngOnInit() {
+  }
+  getStarted()
+  {
+    this.router.navigate(['login']);
+  }
+  // var dateToday = new Date();
+  // page.currentYear = dateToday.getFullYear();
+  async displaypopOver(even: Event )
+  {
+
+    this.userAPiService.displayPopOver('termsOfService','');
   }
 
 }
