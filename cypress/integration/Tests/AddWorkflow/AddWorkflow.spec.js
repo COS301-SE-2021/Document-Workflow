dda8b6e45b669783853cb1f1aa3d96961516d5ee
u17015741 <54096396+u17015741@users.noreply.github.com>
2021-08-19 22:23:58 +0200

    final testing

--- cypress/integration/Tests/AddWorkflow/AddWorkflow.spec.js
+++ cypress/integration/Tests/AddWorkflow/AddWorkflow.spec.js
@@ -16,10 +16,11 @@ describe("testing the add workflow functionality", () => {
         cy.get('#login').click()
         cy.url().should('eq','http://localhost:8100/home');
         cy.wait(8000);
-        cy.visit('http://localhost:8100/home/addWorkflow');
+        
     });
 
     it("input the data", ()=>{
+        cy.visit('http://localhost:8100/home/addWorkflow');
         cy.get('#workflowName').type('This is a document name');
         cy.get('#workflowDescription').type("This is the workflow description");
         cy.wait(10000);
@@ -35,10 +36,59 @@ describe("testing the add workflow functionality", () => {
         cy.get('ion-button').eq(1).click();
         cy.wait(10000);
 
-        cy.wait(5000);
         cy.get('ion-icon').eq(0).click();
         cy.get('ion-button').eq(1).click();
+        cy.wait(14000)
 
+        cy.get('ion-card-title').eq(6).should('contain', 'This is a document name');
     });
-    
+});
+
+describe("testing the delete workflow functionality", () => {   
+    before(() =>{
+        cy.clearCookies();
+        cy.visit('http://localhost:8100');
+        cy.get('#loginEmail').type('brenton.stroberg@yahoo.co.za')
+        cy.get('#loginPassword').type('Password#1')
+        cy.get('#login').click()
+        cy.url().should('eq','http://localhost:8100/home');
+        cy.wait(8000);
+    });
+});
+
+describe("testing the delete workflow functionality", () => {   
+    before(() =>{
+        cy.clearCookies();
+        cy.visit('http://localhost:8100');
+        cy.get('#loginEmail').type('brenton.stroberg@yahoo.co.za')
+        cy.get('#loginPassword').type('Password#1')
+        cy.get('#login').click()
+        cy.url().should('eq','http://localhost:8100/home');
+        cy.wait(8000);
+    });
+
+    it("Should delete the created workflow", ()=>{
+        cy.get(':nth-child(7) > ion-reorder.md > ion-card.md > .header-md > .fab-horizontal-end > :nth-child(1) > .md').click();
+        cy.get(':nth-child(7) > ion-reorder.md > ion-card.md > .header-md > .fab-horizontal-end > .fab-list-side-down > :nth-child(3) > .md').click();
+        cy.wait(250);
+        cy.get('div > ion-grid.md > ion-row.md > :nth-child(1)').click();
+        cy.get('div > ion-grid.md > ion-row.md > :nth-child(1) > .md').click();
+        cy.wait(10000);
+    });
+});
+
+describe("testing the delete workflow functionality", () => {   
+    before(() =>{
+        cy.clearCookies();
+        cy.visit('http://localhost:8100');
+        cy.get('#loginEmail').type('brenton.stroberg@yahoo.co.za')
+        cy.get('#loginPassword').type('Password#1')
+        cy.get('#login').click()
+        cy.url().should('eq','http://localhost:8100/home');
+        cy.wait(8000);
+    });
+
+    it("Check to see if it has added correctly",()=>{
+        cy.get('ion-card-title').eq(6).should('to.not.contain', 'This is a document name');
+    });
 });
