9a4110653ecff5889cd868a9f89327d84b88824c
delareytuks <u14011639@tuks.co.za>
2021-08-26 03:33:50 +0200

    created prototype version of Sanitize function, untested currently

--- src/security/Sanitize.ts
+++ src/security/Sanitize.ts
@@ -1,5 +1,12 @@
 import { sanitize } from "mongo-sanitize";
 
+/**
+ * @description This method sanitizes the input from the request object
+ * to mitigate SQL injection attacks
+ * @param req The request object
+ * @param res The response object
+ * @param next The next method
+ */
 export default async function sanitizeRequest(req, res, next): Promise<void>{
     if(req.body) sanitize(req.body);
     if(req.token) sanitize(req.token);
@@ -7,4 +14,17 @@ export default async function sanitizeRequest(req, res, next): Promise<void>{
     if(req.user) sanitize(req.user);
     if(req.token) sanitize(req.token);
     next();
+}
+
+//TODO: test this properly
+const sanitize = async (input: String) => {
+    input.replace("\'", "");
+    input.replace('\"', "");
+    input.replace("\\", "");
+    input.replace("\/", "");
+    input.replace("$", "\$");
+    input.replace("[", "\[");
+    input.replace("]", "\]");
+    input.replace(".", "\.");
+    input.replace(">", "\>");
 }
