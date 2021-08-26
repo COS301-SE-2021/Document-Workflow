81dbbe42066de799781bca85fab95f5cb23c1a2f
delareytuks <u14011639@tuks.co.za>
2021-08-26 03:28:52 +0200

    Authenticate middleware now checks a blacklist for logged out user tokens, and no longer queries the database when authenticating a token. A fix for the locally caught exceptions is still being researched

--- src/security/Authenticate.ts
+++ src/security/Authenticate.ts
@@ -1,29 +1,45 @@
-import { injectable } from "tsyringe";
+import { autoInjectable } from "tsyringe";
 import UserService from "../user/UserService";
 import jwt from "jsonwebtoken";
 import { AuthenticationError } from "../error/Error";
+import Database from "../Database";
+import { handleErrors } from "../error/ErrorHandler";
 
-@injectable()
+@autoInjectable()
 export default class Authenticator {
-
-    constructor(private userService: UserService) {
+    constructor() {
     }
-
     public async Authenticate(req, res, next) {
-        const { headers } = req;
-        req.on('error', (err) => {
-            console.log(err);
-        });
-        if(!headers.Authorization){
-            next();
-        }
-        //TODO: check if headers are present:
-        const token = req.header("Authorization").replace("Bearer ", "");
-        const decoded = jwt.verify(token, process.env.SECRET);
+        try {
+            const {headers} = req;
+            req.on('error', (err) => {
+                console.log(err);
+            });
+            if (!headers.authorization) {
+                throw new AuthenticationError("User could not be authenticated");
+            }
+            const token = req.header("Authorization").replace("Bearer ", "");
 
-        const user = await this.userService.getUserById(decoded.id); //NOTE: the decoded object has an id field, not a _id field
-        if (!user) throw new AuthenticationError("User could not be authenticated");
-        req.user = user;
-        next();
+            //check blacklist for token first:
+            try{
+                if (await Database.checkBlacklist(token)) {
+                    throw new AuthenticationError("Session has expired");
+                }
+            }catch(err){
+                throw err;
+            }
+
+            let decoded;
+            try {
+                decoded = jwt.verify(token, process.env.SECRET);
+            } catch (err) {
+                throw new AuthenticationError("Invalid access token");
+            }
+
+            req.user = {email: decoded.email, _id: decoded.id};
+            next();
+        }catch(err){
+            await handleErrors(err, res);
+        }
     }
 }
