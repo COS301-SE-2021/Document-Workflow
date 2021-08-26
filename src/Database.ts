5300c279692d33c16dd22c9f48a9b47f2427860e
delareytuks <u14011639@tuks.co.za>
2021-08-26 03:29:51 +0200

    Blacklist functionality was added to the Database class, currently a simple static variable is used for the blacklist but this might be updated to use a redis database later

--- src/Database.ts
+++ src/Database.ts
@@ -2,10 +2,14 @@ import mongoose from "mongoose";
 import { singleton } from "tsyringe";
 import dotenv from "dotenv";
 dotenv.config();
+import { createBlackList } from "jwt-blacklist";
+import { Jwt } from "jsonwebtoken";
+
 
 @singleton()
 export default class Database{
     private static db = null;
+    private static blacklist = null;
 
     private static async connect(): Promise<void> {
         try{
@@ -19,9 +23,25 @@ export default class Database{
             Database.db.once('open', () => {
                  console.log("Connection opened");
              });
+            this.blacklist = await createBlackList({
+                daySize: 10000 | parseInt(process.env.DAY_SIZE),
+                errorRate: 0.001 | parseFloat(process.env.ERROR_RATE)
+            })
         } catch (err) {
             console.error(err);
         }
+    }
+
+    static async addToBlacklist(token: Jwt): Promise<Boolean> {
+        await this.get();
+        if(!await this.blacklist.has(token)){
+            return await this.blacklist.add(token);
+        }
+    }
+
+    static async checkBlacklist(token: Jwt): Promise<Boolean>{
+        await this.get();
+        return await this.blacklist.has(token);
     }
 
     static async get(): Promise<void> {
@@ -29,7 +49,8 @@ export default class Database{
             if(Database.db === null){
                 await this.connect();
             } else {
-                console.log("Connection already exists");
+                if(process.env.TEST_MODE)
+                    console.log("Connection already exists");
             }
         } catch (err) {
             return err;
