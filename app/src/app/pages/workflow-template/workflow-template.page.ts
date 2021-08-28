4d9fff42ee7a959088937a12be21bfef218374f8
u17015741 <54096396+u17015741@users.noreply.github.com>
2021-08-28 17:25:49 +0200

    the changing of template data has been done

--- app/src/app/pages/workflow-template/workflow-template.page.ts
+++ app/src/app/pages/workflow-template/workflow-template.page.ts
@@ -1,7 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Platform } from '@ionic/angular';
+import { ModalController, Platform } from '@ionic/angular';
+import { DocumentActionAreaComponent } from 'src/app/components/document-action-area/document-action-area.component';
 import { phaseUser } from 'src/app/Services/Document/document-api.service';
 import { Logger } from 'src/app/Services/Logger';
 import { UserAPIService } from 'src/app/Services/User/user-api.service';
@@ -19,7 +20,11 @@ export class WorkflowTemplatePage implements OnInit {
   sizeMe: boolean;
   templateForm: FormGroup;
   readyForPhase2: boolean = false;
+  phase2: boolean = true;
   tempDesc: templateDescription[]=[];
+  phaseViewers: boolean[]=[];
+  originalFile:string ;
+  ownerEmail:string;
 
   constructor(
     private fb: FormBuilder,
@@ -29,7 +34,8 @@ export class WorkflowTemplatePage implements OnInit {
     private userService: UserAPIService,
     private route: ActivatedRoute,
     private router: Router,
-    private logger: Logger
+    private logger: Logger,
+    private modal: ModalController,
   ) { }
 
   async ngOnInit() {
@@ -39,16 +45,16 @@ export class WorkflowTemplatePage implements OnInit {
       this.sizeMe = true;
     }
 
-    await this.getTemplateData();
-    // await this.getTemplateids();
+    //todo uncomment the comment
+    // await this.getTemplateData();
+    await this.useThisTemplate('612942988ed7c10970592172');
 
-
-    this.templateForm = this.fb.group({
-      templateName: ['', [Validators.required]],
-      templateDescription: ['', [Validators.required]],
-      templateFile: ['', [Validators.required]],
-      phases: this.fb.array([]),
-    });
+    // this.templateForm = this.fb.group({
+    //   templateName: ['', [Validators.required]],
+    //   templateDescription: ['', [Validators.required]],
+    //   templateFile: ['', [Validators.required]],
+    //   phases: this.fb.array([]),
+    // });
     // await this.getWorkflowTemplateData();
   }
 
@@ -75,16 +81,18 @@ export class WorkflowTemplatePage implements OnInit {
   }
 
   async useThisTemplate(id:string){
+    console.log(id);
     this.templateService.getWorkflowTemplateData(id, (response)=>{
       console.log(response);
       let template = response.template;
-      let fileData = response.fileData;
+      this.originalFile = response.fileData;
       this.templateForm = this.fb.group({
         workflowName: [template.documentName,[Validators.required]],
         workflowDescription: [template.workflowDescription, [Validators.required]],
         phases: this.fb.array([]),
       });
       this.fillPhases(template.phases);
+      this.readyForPhase2 = true;
     })
   }
 
@@ -96,8 +104,14 @@ export class WorkflowTemplatePage implements OnInit {
       console.log(this.templateForm)
   }
 
-  fillPhase(phase: phaseFormat){
+  viewPhase(i: number){
+    console.log(this.phaseViewers)
+    console.log(i)
+    this.phaseViewers[i] = !this.phaseViewers[i];
+  }
 
+  fillPhase(phase: phaseFormat){
+    this.phaseViewers.push(false);
     let temp  = this.fb.group({
       annotations:[phase.annotations,[Validators.required]],
       description:[phase.description,[Validators.required]],
@@ -122,4 +136,87 @@ export class WorkflowTemplatePage implements OnInit {
     })
   }
 
+  nextPhase(){
+    this.phase2 = !this.phase2;
+  }
+
+  addUser(form: FormArray, i: number) {
+    form.push(this.createNewUser());
+  }
+
+  createNewUser(): FormGroup {
+    return this.fb.group({
+      user: new FormControl('', [Validators.email, Validators.required]),
+      permission: new FormControl('', [Validators.required]),
+      accepted: new FormControl("false", [Validators.required]),
+    });
+  }
+
+  removeUser(control: FormArray, i: number, j: number) {
+    if (control.length > 1) {
+      control.removeAt(j);
+    } else {
+      if (this.templateForm.controls.phases['controls'].length > 1) {
+        this.removePhase(i, this.templateForm.get('phases')[i]);
+      } else {
+        this.userService.displayPopOver(
+          'Error',
+          'you need at least one user and phase'
+        );
+      }
+    }
+  }
+
+  changePermission(control: any, str: string, i: number) {
+    control.setValue(str);
+  }
+
+  createPhase(i: number): FormGroup {
+    return this.fb.group({
+      description: new FormControl('', Validators.required),
+      annotations: new FormControl('', [Validators.required]),
+      phaseStatus: new FormControl('Create'),
+      showPhases: new FormControl(true),
+      phaseNumber: new FormControl(i),
+      _id: new FormControl(''),
+      users: this.fb.array([
+        this.fb.group({
+          user: new FormControl('', [Validators.email, Validators.required]),
+          permission: new FormControl('', [Validators.required]),
+          accepted: new FormControl("false", [Validators.required]),
+        }),
+      ]),
+    });
+  }
+
+  addPhase() {
+    let phase = this.templateForm.get('phases') as FormArray;
+    phase.push(this.createPhase(phase.length)); //was + 1 but then it seems we skip a phaseNumber
+  }
+
+  removePhase(i: number, phas: phaseFormat) {
+    let phase = this.templateForm.get('phases') as FormArray;
+    if (phase.at(i)['controls'].phaseStatus.value === 'Create') {
+      phase.removeAt(i);
+    } else {
+      phase.at(i)['controls'].phaseStatus.setValue('Delete');
+      phase.at(i)['controls'].showPhases = false;
+    }
+    console.warn(this.templateForm);
+  }
+
+  async includeActionArea(i: number, form: FormControl) {
+    const a = await this.modal.create({
+      component: DocumentActionAreaComponent,
+      componentProps: {
+        file: this.originalFile,
+        ownerEmail: this.ownerEmail,
+        phaseNumber: i,
+      },
+    });
+  }
+
+  submit(){
+
+  }
 }
