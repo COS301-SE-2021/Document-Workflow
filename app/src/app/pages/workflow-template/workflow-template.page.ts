ed5200e644ab07f61ab8f1fbea242cba8ce471bb
u17015741 <54096396+u17015741@users.noreply.github.com>
2021-08-30 23:08:23 +0200

    template should mostly be working

--- app/src/app/pages/workflow-template/workflow-template.page.ts
+++ app/src/app/pages/workflow-template/workflow-template.page.ts
@@ -1,8 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ModalController, Platform } from '@ionic/angular';
+import WebViewer from '@pdftron/webviewer';
 import { DocumentActionAreaComponent } from 'src/app/components/document-action-area/document-action-area.component';
+import { AIService } from 'src/app/Services/AI/ai.service';
 import { phaseUser } from 'src/app/Services/Document/document-api.service';
 import { Logger } from 'src/app/Services/Logger';
 import { UserAPIService } from 'src/app/Services/User/user-api.service';
@@ -17,6 +19,7 @@ import { WorkflowTemplateService, templateDescription, templatePhaseUser } from 
   styleUrls: ['./workflow-template.page.scss'],
 })
 export class WorkflowTemplatePage implements OnInit {
+  @ViewChild('fileInput', { static: false }) fileInput: ElementRef;
   sizeMe: boolean;
   templateForm: FormGroup;
   readyForPhase2: boolean = false;
@@ -25,6 +28,10 @@ export class WorkflowTemplatePage implements OnInit {
   phaseViewers: boolean[]=[];
   originalFile:string ;
   ownerEmail:string;
+  file:any;
+  blob: Blob;
+  srcFile: any;
+  viewerRef: any;
 
   constructor(
     private fb: FormBuilder,
@@ -36,6 +43,7 @@ export class WorkflowTemplatePage implements OnInit {
     private router: Router,
     private logger: Logger,
     private modal: ModalController,
+    private aiService: AIService,
   ) { }
 
   async ngOnInit() {
@@ -160,10 +168,7 @@ export class WorkflowTemplatePage implements OnInit {
     return this.fb.group({
       description: new FormControl('', Validators.required),
       annotations: new FormControl('', [Validators.required]),
-      phaseStatus: new FormControl('Create'),
-      showPhases: new FormControl(true),
-      phaseNumber: new FormControl(i),
-      _id: new FormControl(''),
+      status: new FormControl('Pending', [Validators.required]),
       users: this.fb.array([
         this.fb.group({
           user: new FormControl('', [Validators.email, Validators.required]),
@@ -200,9 +205,113 @@ export class WorkflowTemplatePage implements OnInit {
         phaseNumber: i,
       },
     });
+    await (await a).present();
+    (await a).onDidDismiss().then(async (data) => {
+      // eslint-disable-next-line @typescript-eslint/dot-notation
+      const result = (await data).data['xfdfString'];
+      if (result) {
+        form.setValue(result);
+      } else {
+        //not delete
+      }
+    });
   }
 
-  submit(){
+  async uploadFile(event: EventTarget) {
+    const eventObj: MSInputMethodContext = event as MSInputMethodContext;
+    const target: HTMLInputElement = eventObj.target as HTMLInputElement;
+    this.file = target.files[0];
+    console.log(typeof this.file);
+    console.log('file', await this.file.arrayBuffer());
+    // const buff = response.data.filedata.Body.data; //wut
 
+    this.srcFile = new Uint8Array(await this.file.arrayBuffer());
+
+    this.templateForm.get('workflowFile').setValue(this.file);
+    this.blob = new Blob([this.file], { type: 'application/pdf;base64' });
+    console.log(this.blob.arrayBuffer());
+    const obj = URL.createObjectURL(this.blob);
+    console.log(obj);
+    this.srcFile = obj;
+    // this.addFile = true;
+
+    this.displayWebViewer(this.blob);
+
+    const addDocButton = document.getElementById('uploadFile');
+    addDocButton.parentNode.removeChild(addDocButton);
+  }
+
+  displayWebViewer(blob: Blob){
+    WebViewer({
+      path: './../../../assets/lib',
+      fullAPI:true
+    }, this.viewerRef.nativeElement).then(async instance =>{
+
+      instance.Core.PDFNet.initialize();
+
+      instance.UI.loadDocument(blob, {filename: 'Preview Document'});
+      instance.UI.disableElements(['ribbons']);
+      instance.UI.setToolbarGroup('toolbarGroup-View',false);
+
+      instance.Core.documentViewer.addEventListener('documentLoaded', async ()=>{
+        const PDFNet = instance.Core.PDFNet;
+        const doc = await PDFNet.PDFDoc.createFromBuffer(await this.file.arrayBuffer());
+
+        let extractedText = "";
+
+        const txt = await PDFNet.TextExtractor.create();
+        ;
+        const pageCount = await doc.getPageCount();
+        for(let i=1; i<=pageCount; ++i){
+          const page = await doc.getPage(i);
+          const rect = await page.getCropBox();
+          txt.begin(page, rect); // Read the page.
+          extractedText += await txt.getAsText();
+        }
+        this.aiService.categorizeDocument(extractedText);
+      });
+    });
+  }
+
+  async submit() {
+    console.log('here')
+    console.log(this.templateForm)
+    // await this.createWorkflow();
+  }
+
+
+
+  async createWorkflow() {
+    console.log('Extracting form data ------------------------------');
+    console.log('Name: ', this.templateForm.controls.workflowName.value);
+    console.log(
+      'Description: ',
+      this.templateForm.controls.workflowDescription.value
+    );
+    console.log(this.templateForm);
+    let template = null;
+    if(this.templateForm.controls.templateName !== undefined){
+      template = {templateName: this.templateForm.controls.templateName.value, templateDescription: this.templateForm.controls.templateDescription.value};
+    }
+
+
+    const phases = this.templateForm.controls.phases.value;
+    const name = this.templateForm.controls.workflowName.value;
+    const description = this.templateForm.controls.workflowDescription.value;
+    await this.workflowService.createWorkflow(
+      name,
+      description,
+      phases,
+      this.originalFile,
+      template,
+      (response) => {
+        if (response.status === 'success') {
+          this.userService.displayPopOver('Success', 'You have successfully created a workflow');
+          this.router.navigate(['/home']);
+        } else {
+          this.userService.displayPopOver('Error', 'Something has gone wrong, please try again');
+        }
+      }
+    );
   }
 }
