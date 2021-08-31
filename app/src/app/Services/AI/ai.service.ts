b625f59f5ae7f8e8ea8c4420d1bd9e218aa7b118
u17112592 <u17112592@tuks.co.za>
2021-08-31 16:21:01 +0200

    Added the date field to workflow history entries when they are updated.

--- app/src/app/Services/AI/ai.service.ts
+++ app/src/app/Services/AI/ai.service.ts
@@ -1,25 +1,51 @@
 import { Injectable } from '@angular/core';
 import * as natural from 'natural';
 
+const DOCUMENT_TYPES = Object.freeze({EXPENSE:'Expense Report', CONSULTING:'Consulting Contract', EMPLOYMENT:'Employment Contract' });
+
 @Injectable({
   providedIn: 'root'
 })
 export class AIService {
 
+    classifier;
+
 
   constructor() {
-
+    this.classifier = new natural.BayesClassifier();
+    this.addDocumentsToClassifier();
   }
   categorizeDocument(extractedText: string){
-    const tokenizer = new natural.WordTokenizer();
-    const classifier = new natural.BayesClassifier();
-    classifier.addDocument('i am long qqqq', 'buy');
-    classifier.addDocument('buy the q\'s', 'buy');
-    classifier.addDocument('short gold', 'sell');
-    classifier.addDocument('sell gold', 'sell');
-    classifier.train();
 
-    console.log(classifier.classify('i am short silver'));
-    console.log(classifier.classify('i am long copper'));
   }
+
+  addDocumentsToClassifier(){
+    this.data.forEach(pair =>{
+      this.classifier.addDocument(pair[0],pair[1]);
+    })
+  }
+  data = [
+    ['Total Meal Expense', DOCUMENT_TYPES.EXPENSE],
+    ['Reimbursement Due', DOCUMENT_TYPES.EXPENSE],
+    ['Processing of receipts and reimbursements may take 4-6 weeks.', DOCUMENT_TYPES.EXPENSE],
+    ['Please mail your Expense Report form & original receipts', DOCUMENT_TYPES.EXPENSE],
+    ['Dallas NARPM Expense Report', DOCUMENT_TYPES.EXPENSE],
+    ['Itemized Expenses or Description for "Other"', DOCUMENT_TYPES.EXPENSE],
+    ['TOTAL REIMBURSEMENT', DOCUMENT_TYPES.EXPENSE],
+    ['Reimbursement must be approved by a Director. Please submit to the Chapter President and Treasurer', DOCUMENT_TYPES.EXPENSE],
+    ['City of Portland Trip and Expense Report', DOCUMENT_TYPES.EXPENSE],
+    ['Complete budget estimate prior to approval and complete actual after trip. Budget Estimate', DOCUMENT_TYPES.EXPENSE],
+    ['This form does not replace the payroll reimbursement form. You must accompany this form and the payroll reimbursement form for any payroll reimbursement. A copy of this form needs to be kept on file with the department', DOCUMENT_TYPES.EXPENSE],
+    ['OTHER TRAVEL EXPENSES Overnight travel requires pre-approved Travel Application', DOCUMENT_TYPES.EXPENSE],
+    ['ORIGINAL RECEIPTS MUST BE ATTACHED REPORTABLE EXPENSES', DOCUMENT_TYPES.EXPENSE],
+    ['expenses', DOCUMENT_TYPES.EXPENSE],
+    ['expenses', DOCUMENT_TYPES.EXPENSE],
+    ['expenses', DOCUMENT_TYPES.EXPENSE],
+    ['expenses', DOCUMENT_TYPES.EXPENSE],
+
+    ['expenses', DOCUMENT_TYPES.EXPENSE],
+    ['expenses', DOCUMENT_TYPES.EXPENSE],
+
+
+  ];
 }
