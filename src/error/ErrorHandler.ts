cb6e8c6ab57dfbb6bd6727822946fcc4a572a2d0
delareytuks <u14011639@tuks.co.za>
2021-09-02 01:04:21 +0200

    Adjusted error message format

--- src/error/ErrorHandler.ts
+++ src/error/ErrorHandler.ts
@@ -13,46 +13,46 @@ dotenv.config();
 
 export async function handleErrors(err: Error, res){
     let testMode = true;
-    let msg = err.message;
+    let msg = ": " + err.message;
     if(!process.env.TEST_MODE) {
         testMode = false;
         msg = "";
     }
 
     if(err instanceof RequestError){
-        await res.status(400).json({status: "error", data:{}, message: "Invalid Request " + msg});
+        await res.status(400).json({status: "error", data:{}, message: "Invalid Request" + msg});
         if(testMode) console.error(err.message);
     }
     if(err instanceof PhaseError){
-        await res.status(400).json({status: "error", data:{}, message: "A phase contains a user that does not exist " + msg});
+        await res.status(400).json({status: "error", data:{}, message: "A phase contains a user that does not exist" + msg});
         if(testMode) console.error(err.message);
     }
     if(err instanceof AuthenticationError){
-        await res.status(401).json({status: "error", data:{}, message: "Authentication Error " + msg});
+        await res.status(401).json({status: "error", data:{}, message: "Authentication Error" + msg});
         if(testMode) console.error(err.message);
     }
     if(err instanceof AuthorizationError){
-        await res.status(401).send("Unauthorized " + msg);
+        await res.status(401).send("Unauthorized" + msg);
         if(testMode) console.error(err.message);
     }
     if(err instanceof CloudError){
-        await res.status(401).send("Cloud Storage Error " + msg);
+        await res.status(401).send("Cloud Storage Error" + msg);
         if(testMode) console.error(err.message);
     }
     if(err instanceof ServerError){
-        await res.status(500).send("Server Error " + msg);
+        await res.status(500).send("Server Error" + msg);
         if(testMode) console.error(err.message);
     }
     if(err instanceof jwt.TokenExpiredError){
-        await res.status(401).send("Session has expired " + msg);
+        await res.status(401).send("Session has expired" + msg);
         if(testMode) console.error(err.message);
     }
     if(err instanceof DatabaseError){
-        await res.status(401).send("Database error " + msg);
+        await res.status(401).send("Database error" + msg);
         if(testMode) console.error(err.message);
     }
     console.error(err);
     //unsafe
-    await res.status(500).send("Error " + msg);
+    await res.status(500).send("Error" + msg);
 
 }
