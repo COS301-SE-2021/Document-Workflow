7a87fde39e4d8b7f675881dfedb6bf08a54117da
u17112592 <u17112592@tuks.co.za>
2021-09-02 17:25:27 +0200

    Found a way to load in the classifier data from what is returned from the backend.

--- app/src/app/Services/AI/ai.service.ts
+++ app/src/app/Services/AI/ai.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import * as natural from 'natural';
-import {DOCUMENT} from "@angular/common";
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {config} from "../configuration";
 
 const DOCUMENT_TYPES = Object.freeze({EXPENSE:'Expense Report', CONSULTING:'Consulting Contract', EMPLOYMENT:'Employment Contract' });
 
@@ -9,14 +10,44 @@ const DOCUMENT_TYPES = Object.freeze({EXPENSE:'Expense Report', CONSULTING:'Cons
 })
 export class AIService {
 
-    classifier;
+  classifier;
 
-
-  constructor() {
+  constructor( private http: HttpClient) {
     this.classifier = new natural.BayesClassifier();
-  }
-  categorizeDocument(extractedText: string){
+    this.http.get(config.url +'/ai/getClassifier').subscribe((response)=>{
+     console.log('AI Service');
+     console.log(response);
+     this.loadClassifier(response);
+    });
 
+  }
+
+  /**
+   * The natural library only supports the loading of a BayesClassifier through a given filename.
+   * Since we are not passing through an entire file the aim here is to copy over the important
+   * data features to a newly constructed bayesclassifier to bypass this issue. Trust me this was
+   * harder than it looks.
+   * @param response
+   */
+  loadClassifier(response){
+    this.classifier = new natural.BayesClassifier();
+    console.log(this.classifier);
+    const classifierData = JSON.parse(response.data.classifierData);
+    console.log(classifierData);
+    this.classifier.docs = classifierData.docs;
+    this.classifier.features = classifierData.features;
+    this.classifier.events = classifierData.events;
+    this.classifier.lastAdded = classifierData.lastAdded;
+    this.classifier.classifier.classFeatures = classifierData.classifier.classFeatures;
+    this.classifier.classifier.classTotals = classifierData.classifier.classTotals;
+    this.classifier.classifier.totalExamples = classifierData.classifier.totalExamples;
+    console.log(this.classifier);
+  }
+
+  categorizeDocument(extractedText: string){
+    const type = this.classifier.classify(extractedText);
+    console.log('Document of type: ', type);
+    return type;
   }
 
 }
--- src/ai/AIService.ts
+++ src/ai/AIService.ts
@@ -2,13 +2,14 @@ import { injectable } from "tsyringe";
 import * as fs from 'fs';
 import {logger} from "../LoggingConfig";
 
+
 @injectable()
 export class AIService{
     constructor() {
     }
-
+    classifierFilePath = 'src/ai/classifier.json';
     retrieveClassifierData(){
-        const classifierData = fs.readFileSync('src/ai/classifier.json').toString(); //TODO: change this pathname
+        const classifierData = fs.readFileSync(this.classifierFilePath).toString();
         return {status:"success", data: {classifierData: classifierData}, message:""};
     }
 
