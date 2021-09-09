52bf1f2e62e45f30e327e80c4a9bdbb5b4ee53b1
u17015741 <54096396+u17015741@users.noreply.github.com>
2021-09-09 21:51:27 +0200

    starting template for contacts

--- app/src/app/pages/contacts/contacts.page.ts
+++ app/src/app/pages/contacts/contacts.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Platform } from '@ionic/angular';
-
+import * as Cookies from 'js-cookie';
+import { UserAPIService } from 'src/app/Services/User/user-api.service';
 
 @Component({
   selector: 'app-contacts',
@@ -12,14 +14,30 @@ export class ContactsPage implements OnInit {
   sizeMe: boolean;
 
   constructor(
-    private plat: Platform,
+    private plat: Platform,private userApiService: UserAPIService,
+    private router: Router,
   ) { }
 
-  ngOnInit() {
+  async ngOnInit() {
     if(this.plat.width() > 572){
       this.sizeMe = false;
     }else{
       this.sizeMe = true;
+    }
+    if (Cookies.get('token') === undefined) {
+      await this.router.navigate(['/login']);
+      return;
+    } else {
+      this.userApiService.checkIfAuthorized().subscribe(
+        (response) => {
+          console.log('Successfully authorized user');
+        },
+        async (error) => {
+          console.log(error);
+          await this.router.navigate(['/login']);
+          return;
+        }
+      );
     }
   }
 
