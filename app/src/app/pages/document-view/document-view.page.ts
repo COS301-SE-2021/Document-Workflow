437b9e9fdd02023db2aecb67b7e22dc0b42da4cf
Timothy Hill <u17112592@tuks.co.za>
2021-09-12 11:51:50 +0200

    Experiments with altering and reading the metadata of a file.
    Saving this since it can't be stashed forever.

--- app/src/app/pages/document-view/document-view.page.ts
+++ app/src/app/pages/document-view/document-view.page.ts
@@ -78,12 +78,69 @@ export class DocumentViewPage implements OnInit, AfterViewInit {
           annotationUser: this.userEmail,
           fullAPI: true
         }, this.viewerRef.nativeElement).then(async instance =>{
+          await instance.Core.PDFNet.initialize(); //To use pdftron in the non-demo mode supply a licence key here
+            /*Test to add metadata to document */
+          const docorig = await instance.Core.PDFNet.PDFDoc.createFromBuffer(arr);
+          const metadata = await docorig.getDocInfo();
+          console.log("METADATA: ");
+          console.log(await metadata.getKeywords());
+          await metadata.setKeywords("hash");
+          console.log(await metadata.getKeywords());
+          const doc = await docorig.getSDFDoc();
+          doc.initSecurityHandler();
+          doc.lock();
+          console.log('Modifying into dictionary, adding custom properties, embedding a stream...');
+
+          const trailer = await doc.getTrailer(); // Get the trailer
+          const info = await trailer.putDict('Info');
+          info.putString('Producer', 'PDFTron PDFNet');
+
+           // Create a custom inline dictionary within Infor dictionary
+          const customDict = await info.putDict('My Direct Dict');
+          customDict.putNumber('My Number', 100);
+
+          // Now we will change PDF document information properties using SDF API
+
+          // Get the Info dictionary.
+          /*
+          let itr = await trailer.find('Info');
+          let info;
+          if (await itr.hasNext()) {
+            info = await itr.value();
+            // Modify 'Producer' entry.
+            info.putString('Producer', 'PDFTron PDFNet');
+            info.putString('reeee', 'reeeeeeeeeeee');
+
+            // read title entry if it is present
+            itr = await info.find('Author');
+            if (await itr.hasNext()) {
+              const itrval = await itr.value();
+              const oldstr = await itrval.getAsPDFText();
+              info.putText('Author', oldstr + ' - Modified');
+            } else {
+              info.putString('Author', 'Me, myself, and I');
+            }
+          } else {
+            // Info dict is missing.
+            info = await trailer.putDict('Info');
+            info.putString('Producer', 'PDFTron PDFNet');
+            info.putString('Title', 'My document');
+            info.putString('reeee', 'reeeeeeeeeeee');
+          }
+          const customDict = await info.putDict('My Direct Dict');
+          customDict.putNumber('My Number', 100); */
+          const docbuf = await doc.saveMemory(0, '%PDF-1.4'); 
+          let blob2 = new Blob([new Uint8Array(docbuf)], {type: 'application/pdf'});
+            /*   */
 
             await instance.Core.PDFNet.initialize(); //To use pdftron in the non-demo mode supply a licence key here
             this.annotationManager = instance.Core.annotationManager;
             this.documentViewer = instance.Core.documentViewer;
+            const documentTest = await instance.Core.createDocument(blob2, {filename: this.docName});
+            console.log('Document Metadata----------------------------');
+            console.log(await documentTest.getMetadata());
 
-            instance.UI.loadDocument(blob, {filename: this.docName});
+            instance.UI.loadDocument(blob2, {filename: this.docName});
             instance.UI.disableElements(['ribbons']);
             instance.UI.setToolbarGroup('toolbarGroup-View',false);
             instance.UI.setHeaderItems(header =>{
