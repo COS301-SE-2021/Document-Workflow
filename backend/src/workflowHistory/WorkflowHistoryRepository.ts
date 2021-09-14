f7a920c26981a7fd03e9a1a3d428dffea0924a04
delareytuks <u14011639@tuks.co.za>
2021-09-14 06:16:00 +0200

    Updated WorkflowHistoryRepository.ts to not use ts-mongoose

---
+++ backend/src/workflowHistory/WorkflowHistoryRepository.ts
@@ -0,0 +1,29 @@
+import { Types } from "mongoose";
+import { DatabaseError } from "../error/Error";
+import { WorkflowHistory } from "./WorkflowHistory";
+import { IWorkflowHistory } from "./IWorkflowHistory";
+type ObjectId = Types.ObjectId;
+
+export default class WorkflowHistoryRepository{
+
+    async saveWorkflowHistory(workflowHistory: IWorkflowHistory): Promise<ObjectId> {
+        try {
+            const newWorkflowHistory = new WorkflowHistory(workflowHistory);
+            await newWorkflowHistory.save();
+            return newWorkflowHistory._id;
+        }
+        catch(err){
+            throw new DatabaseError("The Document Workflow database could not be reached at this time, please try again later.");
+        }
+    }
+
+    async getWorkflowHistory(id: string): Promise<IWorkflowHistory>{
+        try{
+            return await WorkflowHistory.findById(id);
+        }
+        catch(err){
+            throw new DatabaseError("The Document Workflow database could not be reached at this time, please try again later.");
+        }
+    }
+
+}
