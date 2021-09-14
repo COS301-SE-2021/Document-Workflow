7a37d72f85c21864141dd902046fa0561161fb03
Mosa <letswalomosa@gmail.com>
2021-09-14 09:00:54 +0200

    getUserData Profile-side menu

--- app/src/app/pages/split-view/split-view.page.ts
+++ app/src/app/pages/split-view/split-view.page.ts
@@ -8,7 +8,8 @@ import { UserAPIService } from 'src/app/Services/User/user-api.service';
   styleUrls: ['./split-view.page.scss'],
 })
 export class SplitViewPage implements OnInit {
-
+  user;
+  userEmail;
   constructor(
     private userServices: UserAPIService,
     private router: Router,
@@ -17,10 +18,28 @@ export class SplitViewPage implements OnInit {
   ngOnInit() {
   }
 
+  //Get user data
+  async getUserData(): Promise<void>
+  {
+    await this.userServices.getUserDetails(async (response) => {
+      if (response) {
+        this.user = response.data;
+        this.userEmail = this.user.email;
+        const container = document.getElementById('profileDetails');
+        const div = document.createElement('div');
+        div.innerHTML = this.user.email;
+        container.appendChild(div);
+        console.log(this.userEmail);
+      } else {
+        this.userServices.displayPopOver('Error', 'Failed to get users');
+      }
+    });
+
+  }
+
   logout(){
     this.userServices.logout(()=>{
       this.router.navigate(['login']);
     });
-    
   }
 }
