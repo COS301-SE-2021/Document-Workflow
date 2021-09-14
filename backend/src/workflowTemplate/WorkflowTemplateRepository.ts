40ec5b381213d193eacea0f5ae99a1b33a35786f
delareytuks <u14011639@tuks.co.za>
2021-09-14 06:20:45 +0200

    Updated WorkflowTemplateRepository.ts to use new Interface

---
+++ backend/src/workflowTemplate/WorkflowTemplateRepository.ts
@@ -0,0 +1,39 @@
+import { WorkflowTemplate } from "./WorkflowTemplate";
+import { Types } from "mongoose";
+import { DatabaseError } from "../error/Error";
+import { IWorkflowTemplate } from "./IWorkflowTemplate";
+type ObjectId = Types.ObjectId;
+
+export default class WorkflowTemplateRepository{
+    
+    async saveWorkflowTemplate(workflowTemplate: IWorkflowTemplate): Promise<ObjectId>{
+        try{
+            const newWorkflowTemplate = new WorkflowTemplate(workflowTemplate);
+            const response: IWorkflowTemplate = await newWorkflowTemplate.save();
+            if(response) return response._id;
+            else return null;
+        }
+        catch(err){
+            console.log(err);
+            throw new DatabaseError("Could not save workflow information as template");
+        }
+    }
+
+    async findWorkflowTemplate(templateId): Promise<IWorkflowTemplate>{
+        try{
+            return await WorkflowTemplate.findById(templateId).lean();
+        }
+        catch(err){
+            throw new DatabaseError("Could not find the specified workflow Template.");
+        }
+    }
+
+    async deleteWorkflowTemplate(id: ObjectId) {
+        try {
+            await WorkflowTemplate.deleteOne({_id: id});
+        }
+        catch(err){
+            throw new DatabaseError("The Document Workflow database could not be reached at this time, please try again later.");
+        }
+    }
+}
