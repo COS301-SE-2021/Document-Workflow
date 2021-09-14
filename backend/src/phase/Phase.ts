387a3789e8f22778bb62c0fd9ca9351cc04f4239
delareytuks <u14011639@tuks.co.za>
2021-09-14 06:06:48 +0200

    Phase entity updated to not use ts-mongoose

---
+++ backend/src/phase/Phase.ts
@@ -0,0 +1,14 @@
+import { Schema, model } from "mongoose";
+import { IPhase } from "./IPhase";
+
+export const PhaseStatus = Object.freeze({PENDING:"Pending", INPROGRESS:"InProgress", REJECTED:"Rejected", COMPLETED:"Completed", CREATE: "Create", DELETE: "Delete", EDIT:"Edit"});
+
+export const phaseSchema = new Schema<IPhase>({
+    //users: [{ type: Schema.Types.ObjectId, ref: "User", required: true }],
+    users: { type: String, required: true },
+    annotations: { type: String, required: true },
+    description: { type: String, required: true },
+    status: { type: String, required: true, default: PhaseStatus.PENDING },
+});
+
+export const Phase = model<IPhase>('Phase', phaseSchema);
