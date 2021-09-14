1da3c70b3425189b659a3220287e1f927e13f8c9
delareytuks <u14011639@tuks.co.za>
2021-09-14 06:18:19 +0200

    updated WorkflowRepository.ts to use new interface

---
+++ backend/src/workflow/WorkflowRepository.ts
@@ -0,0 +1,79 @@
+import { Workflow } from "./Workflow";
+import { Types } from "mongoose";
+import { DatabaseError } from "../error/Error";
+import { logger } from "../LoggingConfig";
+import { IWorkflow } from "./IWorkflow";
+type ObjectId = Types.ObjectId;
+
+export default class WorkflowRepository{
+
+    async saveWorkflow(workflow: IWorkflow): Promise<IWorkflow> {
+        try {
+            const newWorkflow = new Workflow(workflow);
+            const response: IWorkflow = await newWorkflow.save();
+            if(response)
+                return response;
+            else return null;
+        }
+        catch(err){
+            throw new DatabaseError("The Document Workflow database could not be reached at this time, please try again later.");
+        }
+    }
+
+    async updateWorkflow(workflow: IWorkflow): Promise<Boolean>{
+        console.log("Updating a workflow to have new values: ");
+        console.log(workflow);
+        try {
+            const response: IWorkflow = await Workflow.updateOne({_id: workflow._id}, workflow).lean();
+            return !!response;
+        }
+        catch(err){
+            throw new DatabaseError("The Document Workflow database could not be reached at this time, please try again later.");
+        }
+    }
+
+
+    async deleteWorkflow(id: ObjectId): Promise<ObjectId> {
+        try {
+            const response: IWorkflow = await Workflow.deleteOne({_id: id}).lean();
+            if(response) return response._id;
+            else return null;
+        }
+        catch(err){
+            throw new DatabaseError("The Document Workflow database could not be reached at this time, please try again later.");
+        }
+    }
+
+    async getWorkflow(id: ObjectId): Promise<IWorkflow>{
+        try{
+            const response: IWorkflow = await Workflow.findById(id).lean();
+            if(response) return response;
+            else return null;
+        }
+        catch(err){
+            throw new DatabaseError("The Document Workflow database could not be reached at this time, please try again later.");
+        }
+    }
+
+    //legacy
+    async addDocumentId(workflowId, documentId) {
+        console.log("Adding documentID: ", documentId, " to workflow of id: ", workflowId);
+        try {
+            await Workflow.updateOne({_id: workflowId}, {$set: {documentId: documentId}}, {upsert: true});
+        }
+        catch(err){
+            throw new DatabaseError("The Document Workflow database could not be reached at this time, please try again later.");
+        }
+    }
+
+    //legacy
+    async addWorkflowHistoryId(workflowId, workflowHistoryId){
+        logger.info("Saving the historyId: " + workflowHistoryId + " to workflow " + workflowId);
+        try {
+            await Workflow.updateOne({_id: workflowId}, {$set: {historyId: workflowHistoryId}}, {upsert: true});
+        }
+        catch(err){
+            throw new DatabaseError("The Document Workflow database could not be reached at this time, please try again later.");
+        }
+    }
+}
