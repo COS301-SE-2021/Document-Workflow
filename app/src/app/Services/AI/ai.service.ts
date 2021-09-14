206b4e95a7ed7805b7b31b1aab6d5642b215c54e
Timothy Hill <u17112592@tuks.co.za>
2021-09-14 08:55:47 +0200

    Begun implementing the functions that will extract features from different types of documents.

--- app/src/app/Services/AI/ai.service.ts
+++ app/src/app/Services/AI/ai.service.ts
@@ -5,7 +5,15 @@ import {config} from '../configuration';
 import * as DecisionTree from 'decision-tree';
 import {DocumentClassifier} from './BagOfWordsClassifier';
 
-const DOCUMENT_TYPES = Object.freeze({EXPENSE:'Expense Report', CONSULTING:'Consulting Contract', EMPLOYMENT:'Employment Contract' });
+const DOCUMENT_TYPES = Object.freeze({EXPENSE:'Expense Report', 
+        CONSULTING:'Consulting Contract', 
+        EMPLOYMENT:'Employment Contract',
+        LEASE: 'Lease Agreement',
+        LOAN: 'Loan Agreement',
+        NDA: 'Non-disclosure Agreement',
+        COVID: 'Covid Screening',
+        TIMESHEET: 'Timesheet',
+        INVOICE: 'Invoice' });
 
 @Injectable({
   providedIn: 'root'
@@ -75,4 +83,35 @@ export class AIService {
     return type;
   }
 
+  identifyActionAreas(text, documentType){
+      const features = this.extractFeatures(text, documentType);
+      console.log(features);
+
+  }
+
+  extractFeatures(text, documentType){
+    let features = {};
+      switch(documentType){
+        case DOCUMENT_TYPES.EXPENSE: console.log("Extracting features for document of type: EXPENSE" );
+        break;  
+        case DOCUMENT_TYPES.CONSULTING: console.log("Extracting features for document of type: CONSULTING" );
+        break;
+        case DOCUMENT_TYPES.LOAN: console.log("Extracting features for document of type: LOAN" );
+        break;
+        case DOCUMENT_TYPES.LEASE: console.log("Extracting features for document of type: LEASE" );
+        break;
+        case DOCUMENT_TYPES.TIMESHEET: console.log("Extracting features for document of type: TIMESHEET" );
+        break;
+        case DOCUMENT_TYPES.COVID: console.log("Extracting features for document of type: COVID" );
+        break;
+        case DOCUMENT_TYPES.INVOICE: console.log("Extracting features for document of type: INVOICE" );
+        break;
+        case DOCUMENT_TYPES.NDA: console.log("Extracting features for document of type: NDA" );
+        break;
+        case DOCUMENT_TYPES.EMPLOYMENT: console.log("Extracting features for document of type: EMPLOYMENT" );
+        break;
+      }
+
+      return features;
+  }
 }
