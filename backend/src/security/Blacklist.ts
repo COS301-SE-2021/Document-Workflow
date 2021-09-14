40aee9fe0243336803435231498c913690c8d908
delareytuks <u14011639@tuks.co.za>
2021-09-14 05:57:12 +0200

    Implemented draft version of in-house jwt blacklisting class

---
+++ backend/src/security/Blacklist.ts
@@ -0,0 +1,32 @@
+import jwt from "jsonwebtoken";
+
+export class Blacklist {
+    private static blacklist: string[];
+
+    private static async removeExpiredTokens(){
+        //TODO: Implement with hashmap or REDIS
+        this.blacklist.forEach((token: string, index, object) => {
+            const decoded: any = jwt.verify(token, process.env.SECRET);
+            if(Date.now() >= decoded.exp * 1000){
+                object.splice(index, 1);
+            }
+        });
+    }
+
+    //not persisting blacklist atm
+    static async createBlacklist(){
+        if(Blacklist.blacklist === undefined){
+            this.blacklist = [];
+        }
+    }
+
+    static async addToBlacklist(token: string){
+        await Blacklist.removeExpiredTokens();
+        Blacklist.blacklist.push(token);
+    }
+
+    static async checkBlacklist(token: string): Promise<boolean>{
+        await Blacklist.removeExpiredTokens();
+        return !(Blacklist.blacklist.indexOf(token) != -1);
+    }
+}
