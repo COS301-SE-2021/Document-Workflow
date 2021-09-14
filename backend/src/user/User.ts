0459f620ab83b53d6c3cd0357731783dcf60c638
delareytuks <u14011639@tuks.co.za>
2021-09-14 06:10:05 +0200

    Updated User entity to not use ts-mongoose, changed ownedWorkflows, workflows and workflowTemplates to reference types

---
+++ backend/src/user/User.ts
@@ -0,0 +1,63 @@
+import { Schema, model } from 'mongoose';
+import { isStrongPassword, isEmail } from "validator";
+import { IUser } from "./IUser";
+
+/**
+ * <p>
+ * Schema that defines the User entity and it used to
+ * create the Mongoose Model of the same name
+ * Options objects for the properties are used to validate their respective properties
+ * <p>
+ * @param Object definition object containing the properties and their options used to create the Schema
+ */
+
+export const userSchema = new Schema<IUser>({
+    name: { type: String, required: [true, "Name is a required field"]},
+    surname: { type: String, required: [true, "Surname is a required field"] },
+    initials: { type: String, required: [true, "Initials is a required field"]},
+    email: {
+        type: String,
+        required: [true, "Email is a required field"],
+        trim: true,
+        lowercase: true,
+        unique: true,
+        validate: {
+            validator: value => {
+                return isEmail(value);
+            },
+            message: props => `${props.value} is not a valid email`
+        }
+    },
+    password: {
+        type: String,
+        required: [true, "Password is a required field"],
+        validate: {
+            validator: value => {
+                return isStrongPassword(value);
+            },
+            message: 'Password is too weak'
+        }
+    },
+    signature: { type: Schema.Types.Buffer, required: [true, "A signature is required"] },
+    validated: { type: Boolean, default: false },
+    validateCode: { type: String, required: true },
+    contacts: [{
+        type: Schema.Types.ObjectId,
+        ref:"User",
+        required: false,
+        validate: {
+            validator: function(value) {
+                return this._id != value;
+            },
+            message: "You can't be your own friend!"
+        }
+    }],
+    contactRequests: [{ type: String, required: false, maxlength: [ parseInt(process.env.MAX_CONTACT_REQUESTS) || 50, "Request limit reached" ]}],
+    blockedList: [{ type: String, required: false, maxlength: [ parseInt(process.env.MAX_BLOCKLIST) || 50, "Blocked list is at capacity" ]}],
+    privilegeLevel: { type: String, enum: ['ADMIN', 'USER'], required: false, immutable: true },
+    ownedWorkflows: [{type: Schema.Types.ObjectId, ref: "Workflow", required: false, maxlength: [ parseInt(process.env.MAX_OWNED_WORKFLOWS) || 50, "Workflow limit reached" ]}],
+    workflows: [{type: Schema.Types.ObjectId, ref: "Workflow", required: false, maxlength: [ parseInt(process.env.MAX_WORKFLOWS) || 500, "This user cannot be a part of further workflows" ]}],
+    workflowTemplates: [{type: Schema.Types.ObjectId, ref: "WorkflowTemplate", required: false, maxlength: [ parseInt(process.env.MAX_WORKFLOW_TEMPLATES) || 50, "Workflow template limit reached" ]}]
+});
+
+export const User = model<IUser>('User', userSchema);
