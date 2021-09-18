44275bd146527f776b7a631a849579beab4e5c5c
delareytuks <u14011639@tuks.co.za>
2021-09-18 21:03:24 +0200

    Put the db.on calls back to before mongoose.connect

--- backend/src/Database.ts
+++ backend/src/Database.ts
@@ -9,16 +9,14 @@ export default class Database{
     private static db = null;
 
     private static async connect(): Promise<void> {
-
+        Database.db = mongoose.connection;
+        Database.db.on('error', console.error.bind(console, 'connection error:'));
+        Database.db.once('open', () => {
+            console.log("Connection opened");
+        });
         try{
             await mongoose.connect(process.env.MONGO_PROD_URI);
-            Database.db = mongoose.connection;
-            Database.db.on('error', console.error.bind(console, 'connection error:'));
-            Database.db.once('open', () => {
-                console.log("Connection opened");
-            });
             await Blacklist.createBlacklist();
-
         } catch (err) {
             console.error(err);
         }
