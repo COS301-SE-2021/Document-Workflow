8bfb2d4905f924e22eb8bd021e9cfedcc2249f73
delareytuks <u14011639@tuks.co.za>
2021-09-19 03:30:52 +0200

    changed logging to log to a file, for clarity in additional console logs

--- backend/src/LoggingConfig.ts
+++ backend/src/LoggingConfig.ts
@@ -1,6 +1,8 @@
 import * as winston from 'winston';
 export const logger = winston.createLogger({
     transports: [
-        new winston.transports.Console()
+        //new winston.transports.Console()
+        new winston.transports.File({ filename: 'info.log', level: 'info'}),
+        new winston.transports.File({ filename: 'error.log', level: 'error'})
     ]
 });
